cb1f57c309d9b14994de2b9ce286ff455259163c
MyungsungKo <rhaudtjd0511@naver.com>
2023-10-31 16:52:05 +0900

    a

--- 3.c
+++ 3.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 #include "myProject.h"
 
-int main()
-{
-  int PrintMyName3(void)
+ int PrintMyName3(void)
   {
    printf("3. my name is [이정웅]\n");
    return 1;
   
   }
 
-
-
-}
