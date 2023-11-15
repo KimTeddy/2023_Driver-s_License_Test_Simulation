d9ff50edd57306c5feaa617737894c79feb60795
TeddyElectronics <kyc20126@gmail.com>
2023-11-15 21:57:23 +0900

    차 이동 알고리즘 완료

--- OpenGL-project/OpenGL-project/linux.cpp
+++ OpenGL-project/OpenGL-project/linux.cpp
@@ -3,17 +3,30 @@
 #else
 #include <GL/glut.h>
 #endif
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #ifdef __LINUX//리눅스 전용 코딩
 
 #endif
 
-int Width, Height;
+#define PI 3.141592
 
+//이거 신경 안 써도 됨
+int Width, Height;
+int mouse_x = 0;//마지막으로 클릭한 위치
+int mouse_y = 0;
+int rotationX = 0, rotationY = 0;
+float view_rotate[16] = { 1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1 };
+
+//이게 전부 자동차 관련
 float dspeed, speed;
-float dx=0, dy=0, dl;
+float dxcar = 0, dycar = 0, xcar = 0, ycar = 0;//이동
+float rcar;//로테이션
 bool acceration = false;
+
 
 void changeSize(int w, int h) {
     if (h == 0)  h = 1;
@@ -33,40 +46,6 @@ void changeSize(int w, int h) {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
-//bool lightingEnabled = true;
-//GLfloat lightPosition[] = { 0.0f, 2.0f, 0.0f, 1.0f };
-//GLfloat lightDiffuse[] = { 1.0, 1.0, 1.0, 1.0f };
-//GLfloat lightAmbient[] = { 0.8f, 0.8f, 0.8f, 1.0f };
-//void LightInit()
-//{
-//    glPushMatrix();
-//    glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
-//    glPopMatrix();
-//    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuse);
-//    glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient);
-//    glLightfv(GL_LIGHT0, GL_SPECULAR, lightAmbient);
-//    // Global ambient light
-//    float GAmbient[] = { 0.3, 0.3, 0.3, 1.0 };
-//    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, GAmbient);
-//    // Enable local viewpoint
-//    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
-//    glShadeModel(GL_SMOOTH);
-//    // Material
-//    GLfloat material_ambient[] = { 0.5, 0.5, 0.5, 1.0 };
-//    GLfloat material_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
-//    GLfloat material_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-//    GLfloat material_shininess[] = { 32.0 };
-//    glMaterialfv(GL_FRONT, GL_DIFFUSE, material_diffuse);
-//    glMaterialfv(GL_FRONT, GL_SPECULAR, material_specular);
-//    glMaterialfv(GL_FRONT, GL_AMBIENT, material_ambient);
-//    glMaterialfv(GL_FRONT, GL_SHININESS, material_shininess);
-//
-//    glEnable(GL_DEPTH_TEST);
-//    glEnable(GL_LIGHTING);
-//    glEnable(GL_LIGHT0);
-//    glEnable(GL_COLOR_MATERIAL);
-//    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
-//}
 
 void idle() {
     //acceration = false;
@@ -82,41 +61,48 @@ void Timer(int val) {
     glutPostRedisplay();
     glutTimerFunc(1, Timer, 0);
 }
-//void draw_axes(float scale)
-//{
-//    glDisable(GL_LIGHTING);
-//
-//    glPushMatrix();
-//    glScalef(scale, scale, scale);
-//
-//    glBegin(GL_LINES);
-//
-//    glColor3f(1.0, 0.0, 0.0);
-//    glVertex3f(.8f, 0.05f, 0.0);  glVertex3f(1.0, 0.25f, 0.0); /* Letter X*/
-//    glVertex3f(0.8f, .25f, 0.0);  glVertex3f(1.0, 0.05f, 0.0);
-//    glVertex3f(0.0, 0.0, 0.0);  glVertex3f(1.0, 0.0, 0.0); /* X axis */
-//
-//    glColor3f(0.0, 1.0, 0.0);
-//    glVertex3f(0.0, 0.0, 0.0);  glVertex3f(0.0, 1.0, 0.0); /* Y axis */
-//
-//    glColor3f(0.0, 0.0, 1.0);
-//    glVertex3f(0.0, 0.0, 0.0);  glVertex3f(0.0, 0.0, 1.0); /* Z axis  */
-//    glEnd();
-//
-//    glPopMatrix();
-//
-//    glEnable(GL_LIGHTING);
-//}
 
 void drawingCar() {//자동차 모델링, 이동, 회전
-
-
+    
+}
+void keyboard(unsigned char key, int x, int y) {
+    switch (key) {
+    case 'q':
+    case 'Q': exit(0); break;
+    case 'r':
+    case 'R': glClearColor(1.0, 0.0, 0.0, 1.0); break;
+    case 'w':
+        /*acceration = true;
+        if (speed < 5)
+            speed += 0.1f;
+        else speed = 0;
+        dspeed += speed;*/
+        speed = 1;
+        dxcar = speed * cos((180-rcar) * PI / 180.0); xcar += dxcar;
+        dycar = speed * sin((180-rcar) * PI / 180.0); ycar += dycar;
+        break;
+    case 's': 
+        break;
+    case 'a': rcar += 3;
+        printf("r=%f\n", rcar);
+        break;
+    case 'd': rcar -= 3;
+        printf("r=%f\n", rcar);
+        break;
+    }
+    glutPostRedisplay();
 }
 
 void drawingCar_test() {//테스트용 자동차 모델링, 이동, 회전
     glPushMatrix();
-    glTranslatef(0.0, -0.5, 0.0);
-    glutSolidCube(1.0);
+    glTranslatef(xcar, 10, ycar);//자동차 이동
+    glRotatef(rcar, 0.0f, 1.0f, 0.0f);//자동차 회전
+    //glRotatef(180, 0.0f, 1.0f, 0.0f);//자동차 회전
+    glBegin(GL_LINES);
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex3f(0.0, 0.0, 0.0);  glVertex3f(-10.0, 0.0, 0.0); /* Z axis  */
+    glEnd();
+    glutWireCube(5.0);
     glPopMatrix();
 }
 void drawScene() {//그릴 물체 전체
@@ -126,7 +112,7 @@ void drawScene() {//그릴 물체 전체
         glTranslatef(0.0, -0.5, 0.0);
         glScalef(100.0, 1.0, 60.0);
         glutSolidCube(1.0);
-
+        
         glPushMatrix();
             glColor3f(0.1, 0.1, 0.1);
             glLineWidth(2);
@@ -134,9 +120,8 @@ void drawScene() {//그릴 물체 전체
         glPopMatrix();
     glPopMatrix();
 
-    glRotatef(dspeed, 0.0f, 1.0f, 0.0f);
-
-    drawingCar();
+    drawingCar_test();
+    //drawingCar();
 
     glColor3f(1.0, 0.0, 0.6);
     glutWireTorus(1, 3, 10, 100);
@@ -145,44 +130,43 @@ void drawScene() {//그릴 물체 전체
 void disp() {
     glClearColor(.9f, .9f, .9f, 1.0f);//배경색
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+    glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+
     glViewport(0, 0, Width, Height);
+
     glPushMatrix();
-    gluLookAt(0.0, 30.0, 60.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0);//카메라 위치/바라보는 초점 위치/카메라 기울임
+        gluLookAt(0.0, 30.0, 60.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0);//카메라 위치/바라보는 초점 위치/카메라 기울임
 
-    drawScene();
+        glPushMatrix();
 
+            glRotatef(rotationX, 1.0, 0.0, 0.0);
+            glRotatef(rotationY, 0.0, 1.0, 0.0);
+            glMultMatrixf(view_rotate);
+            drawScene();
+
+        glPopMatrix();
     glPopMatrix();
+
+    //---------------------------------------------------------------
+
     glViewport(Width* 3/ 5, Height* 3/ 5, Width* 2/ 5, Height* 2/ 5);
+    //glViewport(0, Height / 2, Width/3, Height/2);
     glPushMatrix();
-    gluLookAt(0.0, 90.0, 30.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0);
+        gluLookAt(0.0, 60.0, 10.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0);
 
-    drawScene();
-
+        drawScene();
     glPopMatrix();
+
     glutSwapBuffers();
 }
-void keyboard(unsigned char key, int x, int y) {
-    switch (key) {
-    case 'q':
-    case 'Q': exit(0);break;
-    case 'r':
-    case 'R': glClearColor(1.0, 0.0, 0.0, 1.0); break;
-    case 'w':
-        acceration = true;
-        if (speed <= 5)
-            speed += 0.1f;
-        else speed = 0;
-        dspeed += speed; 
-        break;
-    }
-    glutPostRedisplay();
-}
+
 void mouse(int button, int state, int x, int y)
 {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
     {
+        mouse_x = x;
+        mouse_y = y;
     }
     else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) //마우스 버튼에서 손 뗄 때
     {
@@ -190,9 +174,27 @@ void mouse(int button, int state, int x, int y)
     glutPostRedisplay();
 }
 
+
+void motion(int x, int y)
+{
+    int dx = x - mouse_x;
+    int dy = y - mouse_y;
+
+    rotationX += dy;
+    rotationY += dx;
+
+    float view_rotate_init[16] = { 1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1 };
+    for (int i = 0; i < 16; i++) {
+        view_rotate[i] = view_rotate_init[i];
+    }
+    mouse_x = x;
+    mouse_y = y;
+
+    glutPostRedisplay();
+}
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     /*float v[] = {
       lightDiffuse[0],  lightDiffuse[1],
       lightDiffuse[2],  lightDiffuse[3] };
@@ -200,13 +202,14 @@ int main(int argc, char** argv) {
     glutInitWindowSize(1024, 768);
     glutInitWindowPosition(0, 0);
     glutCreateWindow("Embedded system");
-
+    glEnable(GL_DEPTH_TEST);
     glutDisplayFunc(disp);
     glutReshapeFunc(changeSize);
 
     glutTimerFunc(1, Timer, 0);
     glutKeyboardFunc(keyboard);
     glutMouseFunc(mouse);
+    glutMotionFunc(motion);
     glutIdleFunc(idle);
     glutMainLoop();
 
