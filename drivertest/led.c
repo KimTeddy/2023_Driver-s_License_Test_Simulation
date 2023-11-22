9afbbb5f403260a0003971ee5c7dcde0baff0a6b
MyungsungKo <rhaudtjd0511@naver.com>
2023-11-22 20:06:56 +0900

    led driver making

---
+++ drivertest/led.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+
+#define LED_NUM 8
+#include ¡°led.h¡±
+static unsigned int ledValue = 0;
+static int fd = 0;
+
+
+int ledOnOff(int ledNum, int onOff) {
+	int LEDdat;
+	int i = 1;
+	i = i << ledNum;
+	ledValue = ledValue & (~i);
+	if (onOff != 0) ledValue |= i;
+	LEDdat = write(fd, &ledValue, 4);
+	return LEDdat;
+}
+int ledLibInit(void) {
+	fd = open("/dev/periled¡±, O_WRONLY);
+		ledValue = 0;
+	return fd;
+}
+
+int ledStatus(void) {
+	return ledValue;
+}
+
+int ledLibExit(void) {
+	ledValue = 0;
+	ledOnOff(0, 0);
+	close(fd);
+	return 0;
+}
+
+int ledChange(const char data[]) {
+	pdata = strtol(data, NULL, 16);
+	//printf("LED Setup: 0x%X\n", pdata);
+
+	for (int i = 0; i < MAX_LED_NUM; i++) {
+		if (pdata & (0x01 << i)) {
+			LEDdat = ledOnOff(i, 1);
+			if (LEDdat != 4) 
+				printf("setup error");
+		}
+		else {
+			LEDdat = ledOnOff(i, 0);
+			if (LEDdat != 4) 
+				printf("setup error");
+		}
+	}
+}
