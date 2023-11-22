a0d6bda3e58e817f68a5c6d312c28c12f4b7567b
MyungsungKo <rhaudtjd0511@naver.com>
2023-11-22 17:51:18 +0900

    kms.c 통합
    thread 관련, Peri관련 함수 원형 통합

--- OpenGL-project/OpenGL-project/linux.cpp
+++ OpenGL-project/OpenGL-project/linux.cpp
@@ -1,5 +1,10 @@
 #ifdef __LINUX
 #include "GL/freeglut.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <sys/shm.h>
 #else
 #include <GL/glut.h>
 #endif
@@ -9,10 +14,17 @@
 #include <math.h>
 
 //#ifdef __LINUX//리눅스 전용 코딩
-//
+pthread_t thread_object_1; //스레드 1 for rgb led
+pthread_t thread_object_2; //스레드 2 for btn and led
+pthread_t thread_object_3; //스레드 3 for 7segment
+
 //#endif
 
 #define PI 3.141592
+
+int simuwork = 0; //시뮬레이션 게임(?) 동작 여부 확인변수
+int safetybelt = 0, sidebrake = 0, leftlight = 0, rightlight = 0, emerlight = 0; // 안전벨트, 사이드브레이크, 방향지시등좌, 우, 비상등 변수
+int fnddat; // FND(7segment) 데이터 변수
 
 //이거 신경 안 써도 됨
 int Width, Height;
@@ -597,12 +609,113 @@ void motion(int x, int y)
 }
 
 #ifdef __LINUX//리눅스 전용 코딩
+void* trafLight(void) { // 신호등함수
+    //reset RGB LED state
+    while (1) {
+        if (simuwork == 1) { // repeat if simulation is working
+            //LED ON R0 G100 B 0 green
+            sleep(12)
+                //LED ON R100 G63 B 0 orange
+                sleep(3)
+                //LED ON R100 G0 B 0 red
+                sleep(5)
+        }
+    }
+}
 
+
+void* btncheck(void) { //btn and leds. 버튼입력 확인및 LED동작 함수
+    //reset Button flag state
+    while (1) {
+        if (simuwork == 1) { // repeat if simulation is working
+            switch (stEvent.code)
+            {
+            case KEY_HOME:
+                if (safetybelt == 0) {
+                    safetybelt = 1; leddata = leddata ^ 10000000;
+                }
+                else if (safetybelt == 1) {
+                    safetybelt = 1; leddata = leddata ^ 10000000;
+                }
+                break;
+
+            case KEY_BACK: if (sidebrake == 0) {
+                sidebrake = 1; leddata = leddata ^ 01000000;
+            }
+                         else if (sidebrake == 1) {
+                sidebrake = 1; leddata = leddata ^ 01000000;
+            }
+                         break;
+
+            case KEY_SEARCH: if (leftlight == 0) { // 점멸 구현 방법 생각 필요 thread? process?
+                leftlight = 1; leddata = leddata ^ 00110000;
+            }
+                           else if (leftlight == 1) {
+                leftlight = 1; leddata = leddata ^ 00110000;
+            }
+                           break;
+
+            case KEY_MENU: if (rightlight == 0) { // 점멸 구현 방법 생각 필요 thread? process?
+                rightlight = 1; leddata = leddata ^ 00001100;
+            }
+                         else if (rightlight == 1) {
+                rightlight = 1; leddata = leddata ^ 00001100;
+            }
+                         break;
+
+            case KEY_VOLUMEUP: if (emerlight == 0) { // 점멸 구현 방법 생각 필요 thread? process?
+                emerlight = 1; leddata = leddata ^ 00111100;
+            }
+                             else if (emerlight == 1) {
+                emerlight = 1; leddata = leddata ^ 00111100;
+            }
+                             break;
+
+            case KEY_VOLUMEDOWN:
+                // 초기화면으로 복귀 코드
+                break;
+            }
+        }
+    }
+}
+
+
+void* sevenseg(void) {
+    //reset 7seg state
+    while (1) {
+        if (simuwork == 1) { // repeat if simulation is working
+            //100 표시
+            //구간별 조건문 작성해야함
+        }
+    }
+}
+
+/* 7 seg decoder 만들때 참고.
+14,11,10,6,8,7 => 선택 자리수만 LOW, 외에는 HIGH => 3자리만 사용할것이기에 14,11,10 HIGH고정, 6,8,7번갈아가며 LOW 입력.
+- 표시: 5번핀 high, 1,2,3,4,9,12,13번핀 low
+0 표시: 1,2,4,9,12,13번핀 high, 3,5번핀 low
+1 표시: 4,9번핀 high 1,2,3,5,12,13번핀 low
+2 표시: 1,2,5,9,13번핀 high, 3,4,12번핀 low
+3 표시: 2,4,5,9,13번핀 high, 1,3,12번핀 low
+4 표시: 4,5,9,12번핀 high, 1,2,3,13번핀 low
+5 표시: 2,4,5,12,13번핀 high, 1,3,9번핀 low
+6 표시: 1,2,4,5,12,13번핀 high, 3,9번핀 low
+7 표시: 4,9,12,13번핀high, 1,2,3,5번핀 low
+8 표시: 1,2,4,5,9,12,13번핀 high, 3번핀 low
+9 표시: 2,3,4,5,9,12,13번핀 high, 1번핀 low
+*/
 #endif
 
 int main(int argc, char** argv) {
     #ifdef __LINUX//리눅스 전용 코딩
-    
+    pthread_create(&thread_object_1, NULL, trafLight, NULL);
+    pthread_create(&thread_object_2, NULL, btncheck, NULL);
+    pthread_create(&thread_object_2, NULL, sevenseg, NULL);
+
+    pthread_join(thread_object_1, NULL);
+    pthread_join(thread_object_2, NULL);
+    pthread_join(thread_object_3, NULL);
+
     #endif
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
