2ff736567a603201566c06ebd03b3de4ebf574bc
JW-LEE-1015 <149313973+JW-LEE-1015@users.noreply.github.com>
2023-11-22 19:49:58 +0900

    구간 표시 함수 수정

--- OpenGL-project/OpenGL-project/textlcd.cpp
+++ OpenGL-project/OpenGL-project/textlcd.cpp
@@ -1,15 +1,7 @@
 #include "textlcd.h"
 
-/*
-#define CRS_MAIN 0  //메인 메뉴 단게
-#define CRS_START 1 // 코스 시작 전 대기 (게임 시작 전 -> ~설명 단계~)
-#define CRS_BASIC 2 //기본 조작 단계
-#define CRS_UP 3    //경사로
-#define CRS_JUNCTION 4  //교차로
-#define CRS_PARKING 5   //주차
-#define CRS_EMERGENCY 6 //돌발
-#define CRS_ACCEL 7     //가속 구간
-*/
+
+
 
 void crs_lcd()	//함수 이름 수정필요하면 하세요.
 {
--- OpenGL-project/OpenGL-project/textlcd.h
+++ OpenGL-project/OpenGL-project/textlcd.h
@@ -4,3 +4,13 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+
+#define CRS_MAIN 0  //메인 메뉴 단게
+#define CRS_START 1 // 코스 시작 전 대기 (게임 시작 전 -> ~설명 단계~)
+#define CRS_BASIC 2 //기본 조작 단계
+#define CRS_UP 3    //경사로
+#define CRS_JUNCTION 4  //교차로
+#define CRS_PARKING 5   //주차
+#define CRS_EMERGENCY 6 //돌발
+#define CRS_ACCEL 7     //가속 구간
+#define CRS_END 8	//종료
