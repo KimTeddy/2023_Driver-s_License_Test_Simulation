c69a3fe32b86ec935cd0c8f7856566600408591b
TeddyElectronics <kyc20126@gmail.com>
2023-11-23 21:06:21 +0900

    buzzer 기초 완료

--- drivertest/buzzer/buzzer.h
+++ drivertest/buzzer/buzzer.h
@@ -1,7 +1,13 @@
 #ifndef _BUZZER_H_
 #define _BUZZER_H_
+#include "buzzer_pitch.h"
+#define MAX_SCALE_STEP 8
+
+const int musicScale[MAX_SCALE_STEP];
+
 int buzzerInit(void);
 int buzzerPlaySong(int scale);
 int buzzerStopSong(void);
 int buzzerExit(void);
+
 #endif
--- drivertest/buzzer/buzzertest.c
+++ drivertest/buzzer/buzzertest.c
@@ -5,40 +5,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <dirent.h>
-#define MAX_SCALE_STEP 8
+#include "buzzer.h"
+#include "buzzer_pitch.h"
+
 #define BUZZER_BASE_SYS_PATH "/sys/bus/platform/devices/"
 #define BUZZER_FILENAME "peribuzzer"
 #define BUZZER_ENABLE_NAME "enable"
 #define BUZZER_FREQUENCY_NAME "frequency"
-char gBuzzerBaseSysDir[128]; /// sys/bus/platform/devices/peribuzzer.XX 가 결정됨
-const int musicScale[MAX_SCALE_STEP] =
-    {
-        262, /*do*/ 294, 330, 349, 392, 440, 494, /* si */ 523};
-
-int findBuzzerSysPath()
-{
-    DIR *dir_info = opendir(BUZZER_BASE_SYS_PATH);
-    int ifNotFound = 1;
-
-    if (dir_info != NULL)
-    {
-        while (1)
-        {
-            struct dirent *dir_entry;
-            dir_entry = readdir(dir_info);
-            if (dir_entry == NULL)
-                break;
-            if (strncasecmp(BUZZER_FILENAME, dir_entry->d_name, strlen(BUZZER_FILENAME)) == 0)
-            {
-                ifNotFound = 0;
-                sprintf(gBuzzerBaseSysDir, "%s%s/", BUZZER_BASE_SYS_PATH, dir_entry->d_name);
-            }
-        }
-    }
-    printf("find %s\n", gBuzzerBaseSysDir);
-    return ifNotFound;
-}
-// 버저 경로 찾기: /sys/bus/platform/devices/peribuzzer.XX 의 XX를 결정하는 것
 
 void doHelp(void)
 {
@@ -49,31 +22,10 @@ void doHelp(void)
     printf("off(0)\n");
 }
 
-void buzzerEnable(int bEnable)
-{
-    char path[200];
-    sprintf(path, "%s%s", gBuzzerBaseSysDir, BUZZER_ENABLE_NAME);
-    int fd = open(path, O_WRONLY);
-    if (bEnable)
-        write(fd, &"1", 1);
-    else
-        write(fd, &"0", 1);
-    close(fd);
-}
-
-void setFrequency(int frequency)
-{
-    char path[200];
-    sprintf(path, "%s%s", gBuzzerBaseSysDir, BUZZER_FREQUENCY_NAME);
-    int fd = open(path, O_WRONLY);
-    dprintf(fd, "%d", frequency);
-    close(fd);
-}
-
 int main(int argc, char **argv)
 {
     int freIndex;
-    if (argc < 2 || findBuzzerSysPath())
+    if (argc < 2 || buzzerInit())
     {
         printf("Error!\n");
         doHelp();
@@ -81,7 +33,7 @@ int main(int argc, char **argv)
     }
     freIndex = atoi(argv[1]);
     printf("freIndex :%d \n", freIndex);
-    if (freIndex > MAX_SCALE_STEP)
+    if (freIndex > 0)
     {
         printf(" <buzzerNo> over range \n");
         doHelp();
@@ -89,12 +41,18 @@ int main(int argc, char **argv)
     }
     if (freIndex == 0) // disable
     {
-        buzzerEnable(0);
+        buzzerStopSong();
+    }
+    else if(freIndex == -1){
+        buzzerPlaySong(NOTE_C5); usleep(10000);
+        buzzerPlaySong(NOTE_E5); usleep(10000);
+        buzzerPlaySong(NOTE_G5); usleep(10000);
+        buzzerPlaySong(NOTE_C6); usleep(10000);
+        buzzerStopSong();
     }
     else
     {
-        setFrequency(musicScale[freIndex - 1]);
-        buzzerEnable(1);
+        buzzerStopSong();
     }
     return 0;
 }
