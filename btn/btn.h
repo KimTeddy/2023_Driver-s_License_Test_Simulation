3d7796d8112e719fab4d50f83be5954832ef3445
JW-LEE-1015 <149313973+JW-LEE-1015@users.noreply.github.com>
2023-11-23 20:13:49 +0900

    btn16

--- btn/btn.h
+++ btn/btn.h
@@ -13,11 +13,10 @@
 #include <unistd.h>
 
 #define INPUT_DEVICE_LIST "/dev/input/event"
-#define PROBE_FILE "/procs/bus/input/devices"
+#define PROBE_FILE "/proc/bus/input/devices"
 #define HAVE_TO_FIND_1 "N : Name = \"ecube-button\"\n"
 #define HAVE_TO_FIND_2 "H: Handlers=kbd event"
 #define MESSAGE_ID 1122
-
 
 
 struct BUTTON_MSG_T
