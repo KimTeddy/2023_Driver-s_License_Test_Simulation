d9da41818d75a0567967819538360268aafebae4
bllobll5 <124126192+bllobll5@users.noreply.github.com>
2023-11-23 14:34:45 +0900

    줄에 닿으면 -5반환하는데 줄 걸린 뒤로 계속 -5출력됨 띠용

--- off_course.cpp
+++ off_course.cpp
@@ -1,20 +1,38 @@
-//임시로 만든 -- 사각형이 경로 이탈할 판 만들기
+//임시로 만든 -- 줄에 닿으면 -5반환
 #include <GL/glut.h>
+#include <iostream>
+using namespace std;
 
-// 좌표 배열
-int coordinates[][2] = {
+int squareX = -90; // 작은 사각형의 초기 x 좌표
+int squareY = 0;   // 작은 사각형의 초기 y 좌표
+int squareSize = 10; // 작은 사각형의 크기
+
+// 4개의 루프 선분의 좌표
+int lineCoordinates[][2] = {
     {-97, -61},
     {-97, 55},
     {91, 55},
     {91, -61}
 };
 
-void drawLines() {
-    glBegin(GL_LINE_STRIP);
-    glColor3f(1.0, 1.0, 1.0); // 흰색 선
+bool touchingLoop = false;
 
-    for (int i = 0; i < sizeof(coordinates) / sizeof(coordinates[0]); ++i) {
-        glVertex2i(coordinates[i][0], coordinates[i][1]);
+void drawSquare() {
+    glBegin(GL_QUADS);
+    glColor3f(1.0, 1.0, 1.0); // 흰색 사각형
+    glVertex2i(squareX, squareY);
+    glVertex2i(squareX + squareSize, squareY);
+    glVertex2i(squareX + squareSize, squareY + squareSize);
+    glVertex2i(squareX, squareY + squareSize);
+    glEnd();
+}
+
+void drawLoopLines() {
+    glBegin(GL_LINE_LOOP);
+    glColor3f(1.0, 1.0, 1.0); // 흰색 루프 선
+
+    for (int i = 0; i < sizeof(lineCoordinates) / sizeof(lineCoordinates[0]); ++i) {
+        glVertex2i(lineCoordinates[i][0], lineCoordinates[i][1]);
     }
 
     glEnd();
@@ -22,8 +40,65 @@ void drawLines() {
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
-    drawLines();
-    glFlush();
+
+    // 작은 사각형 그리기
+    drawSquare();
+
+    // 루프 선 그리기
+    drawLoopLines();
+
+    // 작은 사각형이 루프 선에 닿으면 "-5"를 출력
+    if (touchingLoop) {
+        std:cout << "-5" << std::endl;
+    }
+
+    glutSwapBuffers();
+}
+
+void timer(int value) {
+    touchingLoop = false;
+    glutPostRedisplay();
+}
+
+void keyboard(unsigned char key, int x, int y) {
+    // 키보드 입력에 따라 작은 사각형을 움직임
+    switch (key) {
+    case 'w':
+        squareY += 5; // 위로 이동
+        break;
+    case 's':
+        squareY -= 5; // 아래로 이동
+        break;
+    case 'a':
+        squareX -= 5; // 왼쪽으로 이동
+        break;
+    case 'd':
+        squareX += 5; // 오른쪽으로 이동
+        break;
+    }
+
+    // 작은 사각형이 루프 선에 닿으면 1초 동안 "-5"를 출력
+    for (int i = 0; i < sizeof(lineCoordinates) / sizeof(lineCoordinates[0]) - 1; ++i) {
+        int x1 = lineCoordinates[i][0];
+        int y1 = lineCoordinates[i][1];
+        int x2 = lineCoordinates[i + 1][0];
+        int y2 = lineCoordinates[i + 1][1];
+
+        int squareCenterX = squareX + squareSize / 2;
+        int squareCenterY = squareY + squareSize / 2;
+
+        if ((squareCenterY > y1 && squareCenterY <= y2) || (squareCenterY > y2 && squareCenterY <= y1)) {
+            int intersectX = ((squareCenterY - y1) * (x2 - x1)) / (y2 - y1) + x1;
+            if (intersectX <= squareCenterX) {
+                touchingLoop = true;
+                glutTimerFunc(1000, timer, 0); // 1초 후에 타이머 콜백 함수 호출
+                break;
+            }
+        }
+    }
+
+    // 화면을 다시 그림
+    glutPostRedisplay();
 }
 
 void init() {
@@ -34,13 +109,14 @@ void init() {
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("OpenGL Line Drawing");
+    glutCreateWindow("OpenGL Square and Loop Lines");
 
     init();
     glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
     glutMainLoop();
 
     return 0;
