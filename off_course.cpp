78c71b059ef83c29d773c6e5d8fd0ec260027cee
bllobll5 <124126192+bllobll5@users.noreply.github.com>
2023-11-24 22:24:52 +0900

    Update off_course.cpp (too much initializer)

--- off_course.cpp
+++ off_course.cpp
@@ -21,31 +21,26 @@
 //그 2차원 배열 -> 라인 만들 곳! (경로)
 #include <GL/glut.h>
 #include <iostream>
-#include <string>
 
 int squareX = 0; // 작은 사각형의 초기 x 좌표
 int squareY = 50; // 작은 사각형의 초기 y 좌표
 int squareSize = 10; // 작은 사각형의 크기
 
-// 4개의 루프 선분의 좌표
-int lineCoordinates[][2] = {
-    {-97, -61},
-    {-97, 55},
-    {91, 55},
-    {91, -61}
-};
-
-// 추가 선의 좌표
-int additionalLineCoordinates[][2] = {
-    {-73, 9},
-    {-73, 36},
-    {-9, 36},
-    {-9, 9}
+// 10개의 루프 선분의 좌표
+int lineCoordinates[][2][2] = {
+    {{-97, -61}, {-97, 55}, {91, 55}, {91, -61}},
+    {{-73, 9}, {-73, 36}, {-9, 36}, {-9, 9}},
+    {{9, 9}, {9, 36}, {73, 36}, {73, 9}},
+    {{9, -43}, {9, -9}, {73, -9}, {73, -43}},
+    {{-76, -43}, {-73, -9}, {-44, -9}, {-44, -20}, {-32, -20}, {-32, -9}, {-9, -9}, {-9, -43}},
+    {{-97, 26}, {-73, 26}},
+    {{-19, 0}, {-19, 9}},
+    {{67, 0}, {67, 9}},
+    {{9, 9}, {9, -9}},
+    {{-9, -17}, {0, -17}}
 };
 
 bool touchingLoop = false;
-bool displayNegativeFive = false; // -5를 표시할지 여부
-int negativeFiveDisplayDuration = 1000; // -5 표시 지속 시간 (1초)
 
 void drawSquare() {
     glBegin(GL_QUADS);
@@ -58,72 +53,64 @@ void drawSquare() {
 }
 
 void drawLoopLines() {
-    glBegin(GL_LINE_LOOP);
-    glColor3f(1.0, 1.0, 1.0); // 흰색 루프 선
-
     for (int i = 0; i < sizeof(lineCoordinates) / sizeof(lineCoordinates[0]); ++i) {
-        glVertex2i(lineCoordinates[i][0], lineCoordinates[i][1]);
-    }
+        glBegin(GL_LINE_LOOP);
+        glColor3f(1.0, 1.0, 1.0); // 흰색 루프 선
 
-    glEnd();
-}
-
-void drawAdditionalLine() {
-    glBegin(GL_LINE_STRIP);
-    glColor3f(1.0, 1.0, 1.0); // 흰색 선
-
-    for (int i = 0; i < sizeof(additionalLineCoordinates) / sizeof(additionalLineCoordinates[0]); ++i) {
-        glVertex2i(additionalLineCoordinates[i][0], additionalLineCoordinates[i][1]);
-    }
-
-    glEnd();
-}
-
-void drawNegativeFive() {
-    if (displayNegativeFive) {
-        glColor3f(1.0, 1.0, 1.0); // 흰색 텍스트
-        glRasterPos2i(0, 0);
-
-        std::string text = "-5";
-        for (char c : text) {
-            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, c);
+        for (int j = 0; j < sizeof(lineCoordinates[0]) / sizeof(lineCoordinates[0][0]); ++j) {
+            glVertex2i(lineCoordinates[i][j][0], lineCoordinates[i][j][1]);
         }
+
+        glEnd();
     }
 }
 
-void isSquareInsideLoop() {
-    touchingLoop = false;
+bool isPointOnSegment(int px, int py, int x1, int y1, int x2, int y2) {
+    return (px >= std::min(x1, x2) && px <= std::max(x1, x2) &&
+        py >= std::min(y1, y2) && py <= std::max(y1, y2));
+}
 
-    for (int i = 0; i < sizeof(lineCoordinates) / sizeof(lineCoordinates[0]) - 1; ++i) {
-        int x1 = lineCoordinates[i][0];
-        int y1 = lineCoordinates[i][1];
-        int x2 = lineCoordinates[i + 1][0];
-        int y2 = lineCoordinates[i + 1][1];
+bool doIntersect(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+    int o1 = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
+    int o2 = (x2 - x1) * (y4 - y1) - (y2 - y1) * (x4 - x1);
+    int o3 = (x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3);
+    int o4 = (x4 - x3) * (y2 - y3) - (y4 - y3) * (x2 - x3);
 
-        if ((squareX >= std::min(x1, x2) && squareX <= std::max(x1, x2) &&
-            squareY >= std::min(y1, y2) && squareY <= std::max(y1, y2)) ||
-            (squareX + squareSize >= std::min(x1, x2) && squareX + squareSize <= std::max(x1, x2) &&
-                squareY >= std::min(y1, y2) && squareY <= std::max(y1, y2)) ||
-            (squareX >= std::min(x1, x2) && squareX <= std::max(x1, x2) &&
-                squareY + squareSize >= std::min(y1, y2) && squareY + squareSize <= std::max(y1, y2)) ||
-            (squareX + squareSize >= std::min(x1, x2) && squareX + squareSize <= std::max(x1, x2) &&
-                squareY + squareSize >= std::min(y1, y2) && squareY + squareSize <= std::max(y1, y2))) {
-            touchingLoop = true;
-            break;
-        }
+    if ((o1 * o2 < 0) && (o3 * o4 < 0)) {
+        return true;
     }
+
+    if (o1 == 0 && isPointOnSegment(x3, y3, x1, y1, x2, y2)) return true;
+    if (o2 == 0 && isPointOnSegment(x4, y4, x1, y1, x2, y2)) return true;
+    if (o3 == 0 && isPointOnSegment(x1, y1, x3, y3, x4, y4)) return true;
+    if (o4 == 0 && isPointOnSegment(x2, y2, x3, y3, x4, y4)) return true;
+
+    return false;
 }
 
 void checkIntersection() {
-    isSquareInsideLoop();
+    touchingLoop = false;
 
-    // 사각형이 루프 선에 닿으면 "-5" 표시 및 타이머 설정
-    if (touchingLoop) {
-        displayNegativeFive = true;
-        glutTimerFunc(negativeFiveDisplayDuration, [](int value) {
-            displayNegativeFive = false;
-            }, 0);
+    for (int i = 0; i < sizeof(lineCoordinates) / sizeof(lineCoordinates[0]); ++i) {
+        for (int j = 0; j < sizeof(lineCoordinates[0]) / sizeof(lineCoordinates[0][0]) - 1; ++j) {
+            int x1 = lineCoordinates[i][j][0];
+            int y1 = lineCoordinates[i][j][1];
+            int x2 = lineCoordinates[i][j + 1][0];
+            int y2 = lineCoordinates[i][j + 1][1];
+
+            if (doIntersect(squareX, squareY, squareX, squareY + squareSize, x1, y1, x2, y2)) {
+                touchingLoop = true;
+                return;
+            }
+        }
     }
+}
+
+void drawNegativeFive() {
+    glColor3f(1.0, 0.0, 0.0); // 빨간색 텍스트
+    glRasterPos2i(0, 0); // 텍스트 위치
+    glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, '-');
+    glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, '5');
 }
 
 void display() {
@@ -135,14 +122,13 @@ void display() {
     // 루프 선 그리기
     drawLoopLines();
 
-    // 추가 선 그리기
-    drawAdditionalLine();
-
     // 작은 사각형이 루프 선에 닿으면 "-5"를 출력
     checkIntersection();
 
     // "-5" 표시
-    drawNegativeFive();
+    if (touchingLoop) {
+        drawNegativeFive();
+    }
 
     glutSwapBuffers();
 }
