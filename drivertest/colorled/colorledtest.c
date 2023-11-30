67a8ca83abe7a7c2a0ae1316283ac6d31fb7bcce
JW-LEE-1015 <149313973+JW-LEE-1015@users.noreply.github.com>
2023-11-30 20:17:03 +0900

    color

--- drivertest/colorled/colorledtest.c
+++ drivertest/colorled/colorledtest.c
@@ -2,14 +2,14 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc != 4)
-    {
-        printf("ERR\r\n");
-        printf("colorledtest RED GREEN BLUE\r\n");
-        printf("ex) ./colorledtest 0~100 0~100 0~100\r\n");
+    //if(argc != 4)
+   // {
+   //     printf("ERR\r\n");
+  //      printf("colorledtest RED GREEN BLUE\r\n");
+  //      printf("ex) ./colorledtest 0~100 0~100 0~100\r\n");
 
-        return 0;
-    }
+  //      return 0;
+  //  }
     pwmLedInit();
     
     //pwmSetPercent(atoi(argv[1]), 0);
@@ -18,55 +18,57 @@ int main(int argc, char *argv[])
     
     //pwmSetPercent(atoi(argv[3]), 2);
     // argv로 받은 R,G,B값으로 LED를 On.
-    
+    while(1)
+    {
     //빨
     pwmSetPercent(100, 0);
     pwmSetPercent(0, 1);
     pwmSetPercent(0, 2);
-    usleep(1);
+    //usleep(1);
 
     //주
     pwmSetPercent(100, 0);
     pwmSetPercent(50, 1);
     pwmSetPercent(0, 2);
-    usleep(1);
+    //usleep(1);
 
     //노
     pwmSetPercent(100, 0);
     pwmSetPercent(100, 1);
     pwmSetPercent(0, 2);
-    usleep(1);
+    //usleep(1);
 
     //초
     pwmSetPercent(0, 0);
     pwmSetPercent(100, 1);
     pwmSetPercent(0, 2);
-    usleep(1);
+   // usleep(1);
 
     //파
     pwmSetPercent(0, 0);
     pwmSetPercent(0, 1);
     pwmSetPercent(100, 2);
-    usleep(1);
+    //usleep(1);
+
     //남
     pwmSetPercent(0, 0);
     pwmSetPercent(0, 1);
     pwmSetPercent(50, 2);
-    usleep(1);
+   // usleep(1);
 
     //보
     pwmSetPercent(50, 0);
     pwmSetPercent(0, 1);
     pwmSetPercent(50, 2);
-    usleep(1);
+   // usleep(1);
 
     //흰
     pwmSetPercent(100, 0);
     pwmSetPercent(100, 1);
     pwmSetPercent(100, 2);
-    usleep(1);
-    
-    //while(1);
+    //usleep(1);
+
+    }
     pwmInactiveAll();
     return 0;
 }
