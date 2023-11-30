eb1a536fbdeee16b406edf4ea6240ff279b58b82
JW-LEE-1015 <149313973+JW-LEE-1015@users.noreply.github.com>
2023-11-30 20:22:04 +0900

    색 보정

--- drivertest/colorled/colorledtest.c
+++ drivertest/colorled/colorledtest.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
     //노
     pwmSetPercent(100, 0);
     pwmSetPercent(100, 1);
-    pwmSetPercent(0, 2);
+    pwmSetPercent(50, 2);
     sleep(1);
 
     //초
@@ -46,20 +46,20 @@ int main(int argc, char *argv[])
 
     //파
     pwmSetPercent(0, 0);
-    pwmSetPercent(0, 1);
+    pwmSetPercent(50, 1);
     pwmSetPercent(100, 2);
     sleep(1);
 
     //남
     pwmSetPercent(0, 0);
     pwmSetPercent(0, 1);
-    pwmSetPercent(50, 2);
+    pwmSetPercent(100, 2);
     sleep(1);
 
     //보
     pwmSetPercent(50, 0);
     pwmSetPercent(0, 1);
-    pwmSetPercent(50, 2);
+    pwmSetPercent(100, 2);
     sleep(1);
 
     //흰
