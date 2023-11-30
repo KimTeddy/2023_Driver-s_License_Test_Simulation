9f0a401cbd3e53153a4c922d4f8e513baf9dee28
bllobll5 <124126192+bllobll5@users.noreply.github.com>
2023-11-30 21:25:29 +0900

    Update lcdtext.c

--- textlcd_homework/lcdtext.c
+++ textlcd_homework/lcdtext.c
@@ -12,8 +12,11 @@ void lcdtextwrite(const char *str1, const char *str2, int lineFlag) {
     // 전달된 문자열을 textlcd 배열에 복사
     if (lineFlag == 1) {
         snprintf(textlcd[0], sizeof(textlcd[0]), "%s", str1);
+       [NUM_ROWS][NUM_COLS + 1] = {0};
+       textlcd[NUM_ROWS][1] = {0};
     } else if (lineFlag == 2) {
         snprintf(textlcd[1], sizeof(textlcd[1]), "%s", str2);
+        textlcd[NUM_ROWS][0] = {0};
     } else {
         printf("choose 1 or 2\n");
         return;
