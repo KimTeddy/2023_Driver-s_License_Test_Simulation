e42d7d407a9861177de4cad723046ccc93b338ed
bllobll5 <124126192+bllobll5@users.noreply.github.com>
2023-11-30 21:13:27 +0900

    Update and rename textlcd.c to lcdtext.h

---
+++ drivertest/textlcd/lcdtext.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef _TEXTLCD_H_
+#define _TEXTLCD_H_
+#define CMD_WRITE_STRING 0x20
+#define CMD_DATA_WRITE_BOTH_LINE 0
+#define CMD_WRITE_STRING 0x20
+#define CMD_DATA_WRITE_BOTH_LINE 0
+#define NUM_ROWS 2
+#define NUM_COLS 16
+
+void lcdtextwrite(const char *str1, const char *str2, int lineFlag);
+
+
+#endif
--- drivertest/textlcd/textlcd.c
+++
@@ -1,1 +0,0 @@
-#include "textlcd.h"
