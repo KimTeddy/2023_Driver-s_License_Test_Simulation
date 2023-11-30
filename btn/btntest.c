3dfc28d0c065398b32e27f03b1689f54159f4322
JW-LEE-1015 <149313973+JW-LEE-1015@users.noreply.github.com>
2023-11-30 17:59:17 +0900

    cp

--- btn/btntest.c
+++ btn/btntest.c
@@ -1,29 +1,29 @@
 #include "btn.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    //buttonInit();
-   // int fp;
-    //int readSize, inputIndex;
-   // struct input_event stEvent;
-    //char inputDevPath[200] = {0, };
-
-    buttonInit();
+    int fp;
+    int readSize, inputIndex;
     struct input_event stEvent;
-    int msgID = msgget(MESSAGE_ID, IPC_CREAT|0666);
-    if(msgID == -1)
-    {
-        printf("Can't find\n");
-        return 1;
-    }
+    char inputDevPath[200]={0, };
 
+    if(probeButtonPath(inputDevPath) == 0)
+    {
+        printf("ERR FILE NOT FOUND\r\n");
+        printf("INSMOD\r\n");
+        return 0;
+    }
+    printf("inputDevPath: %s\r\n", inputDevPath);
+    fp = open(inputDevPath, O_RDONLY);
     while(1)
     {
+        readSize = read(fp, &stEvent, sizeof(stEvent));
+        if(readSize != sizeof(stEvent))
+        {
+            continue;
+        }
 
-        int retmsg = 0;
-        retmsg = msgrcv(msgID, &stEvent, sizeof(stEvent)-sizeof(long int), 0, 0);
-
-        if(stEvent.type == EV_KEY)
+        if( stEvent.type == EV_KEY)
         {
             printf("EV_KEY(");
             switch(stEvent.code)
@@ -31,17 +31,15 @@ int main()
                 case KEY_VOLUMEUP : printf("Volume up key):"); break;
                 case KEY_HOME : printf("Home key):"); break;
                 case KEY_SEARCH : printf("Search key):"); break;
-                case KEY_BACK: printf("Back key):"); break;
-                case KEY_MENU : printf("Menu key):"); break;
-                case KEY_VOLUMEDOWN : printf("Volume down key):"); break;
+                case KEY_BACK : printf("Back key):"); break;
+                case KEY_MENU : printf("Menu key)"); break;
+                case KEY_VOLUMEDOWN : printf("Volume down key)"); break;
             }
-            if(stEvent.value) printf("Pressed!\n");
+            if(stEvent.value) printf("pressed\n");
             else printf("released\n");
         }
-        else 
-        ;  //doNothing
-    }   //While End
-    //close(fp);
-    buttonExit();
-
+        else
+        ;
+    }
+    close(fp);
 }
