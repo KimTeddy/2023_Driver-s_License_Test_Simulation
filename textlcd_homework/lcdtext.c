29f8feaa18bce51fbefd78418b3ca73514f37b63
bllobll5 <124126192+bllobll5@users.noreply.github.com>
2023-11-30 20:55:46 +0900

    Update lcdtext.c

--- textlcd_homework/lcdtext.c
+++ textlcd_homework/lcdtext.c
@@ -1,52 +1,24 @@
 // lcdtext.c
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <fcntl.h>
-#include <linux/fb.h>
+
 #include "lcdtext.h"
 
 // LCD 디바이스의 파일 디스크립터
-int lcd_fd;
 
-int main() {
-    // LCD 장치 파일 열기
-    lcd_fd = open("/dev/peritextlcd", O_RDWR);
-    if (lcd_fd == -1) {
-        perror("Error opening LCD device");
-        return 1;
-    }
-//////////////////////////////////////////////////////////////
-    char str1[NUM_COLS + 1];
-    char str2[NUM_COLS + 1];
-    int lineFlag;
 
-    // 사용자로부터 입력 받기
-    printf("Enter line number (1 or 2) and 'text' : ");
-    scanf("%d ", &lineFlag);  // 숫자 입력
-    fgets(lineFlag == 1 ? str1 : str2, sizeof(str1), stdin);
+void lcdtextwrite(const char *str1, const char *str2, int lineFlag) {
+    char textlcd[NUM_ROWS][NUM_COLS + 1] = {0};
 
-    // 문자열에서 개행 문자 제거
-    size_t len1 = strlen(str1);
-    if (len1 > 0 && str1[len1 - 1] == '\n') {
-        str1[len1 - 1] = '\0';
+    // 전달된 문자열을 textlcd 배열에 복사
+    if (lineFlag == 1) {
+        snprintf(textlcd[0], sizeof(textlcd[0]), "%s", str1);
+    } else if (lineFlag == 2) {
+        snprintf(textlcd[1], sizeof(textlcd[1]), "%s", str2);
+    } else {
+        printf("choose 1 or 2\n");
+        return;
     }
 
-    size_t len2 = strlen(str2);
-    if (len2 > 0 && str2[len2 - 1] == '\n') {
-        str2[len2 - 1] = '\0';
-    }
-
-    // lcdtextwrite 함수 호출
-    lcdtextwrite(str1, str2, lineFlag);
-
-    // LCD 장치 파일 닫기
-    close(lcd_fd);
-
-    return 0;
 }
 
 
