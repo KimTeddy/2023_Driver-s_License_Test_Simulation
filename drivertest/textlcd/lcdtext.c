d58cd6e2065c426c17cc74ce68bb90166b97b52a
bllobll5 <124126192+bllobll5@users.noreply.github.com>
2023-11-30 22:12:46 +0900

    Update lcdtext.c

--- drivertest/textlcd/lcdtext.c
+++ drivertest/textlcd/lcdtext.c
@@ -6,7 +6,7 @@ void lcdtextwrite(const char *str1, const char *str2, int lineFlag) {
     // 전달된 문자열을 textlcd 배열에 복사
     if (lineFlag == 1) {
         snprintf(textlcd[0], sizeof(textlcd[0]), "%s", str1);
-        
+        textlcd[1][0] = '\0';
     } else if (lineFlag == 2) {
         snprintf(textlcd[1], sizeof(textlcd[1]), "%s", str2);
     } else {
