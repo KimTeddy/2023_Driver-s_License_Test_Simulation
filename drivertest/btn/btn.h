bdb788ebb2d611d67ba0725532e70291c337eda4
MyungsungKo <rhaudtjd0511@naver.com>
2023-12-01 15:51:34 +0900

    끼야아아악

--- drivertest/btn/btn.h
+++ drivertest/btn/btn.h
@@ -1,7 +1,5 @@
 #ifndef _BTN_H_
 #define _BTN_H_
-#define MESSAGE_ID 1122
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,11 +11,8 @@
 #include <sys/msg.h>
 #include <pthread.h>
 
-// first read input device
-#define INPUT_DEVICE_LIST "/dev/input/event" //실제 디바이스 드라이버 노드파일: 뒤에 숫자가 붙음., ex)/dev/input/event5
-#define PROBE_FILE "/proc/bus/input/devices"  // PPT에 제시된 "이 파일을 까보면 event? 의 숫자를 알수 있다"는 바로 그 파일
-#define HAVE_TO_FIND_1 "N: Name=\"ecube-button\"\n"
-#define HAVE_TO_FIND_2 "H: Handlers=kbd event"
+#define MESSAGE_ID 1122
+
 
 typedef struct
 {
@@ -29,6 +24,5 @@ typedef struct
 int buttonInit(void);
 int buttonExit(void);
 int probeButtonPath(char *newPath);
-int buttonStatus(void);
 static void *buttonThFunc(void);
 #endif
--- drivertest/btn/btntest.c
+++ drivertest/btn/btntest.c
@@ -2,49 +2,33 @@
 
 int main(int argc, char *argv[])
 {
-    int fp;
-    int readSize, inputIndex;
-    struct input_event stEvent;
-    char inputDevPath[200]={0, };
 
-    if(probeButtonPath(inputDevPath) == 0)
-    {
-        printf("ERR FILE NOT FOUND\r\n");
-        printf("INSMOD\r\n");
-        return 0;
-    }
-    printf("inputDevPath: %s\r\n", inputDevPath);
-    fp = open(inputDevPath, O_RDONLY);
-    
-    while(1)
-    {
-        readSize = read(fp, &stEvent, sizeof(stEvent));
-        if(readSize != sizeof(stEvent))
-        {
-            printf("if ERR\n");
-            printf("readSize : %d\n", readSize);
-            printf("sizeof stEvent : %d\n", sizeof(stEvent));
-            sleep(5);
-            continue;
-        }
 
-        if( stEvent.type == EV_KEY)
-        {
-            printf("EV_KEY(");
-            switch(stEvent.code)
-            {
-                case KEY_VOLUMEUP : printf("Volume up key):"); break;
-                case KEY_HOME : printf("Home key):"); break;
-                case KEY_SEARCH : printf("Search key):"); break;
-                case KEY_BACK : printf("Back key):"); break;
-                case KEY_MENU : printf("Menu key)"); break;
-                case KEY_VOLUMEDOWN : printf("Volume down key)"); break;
-            }
-            if(stEvent.value) printf("pressed\n");
-            else printf("released\n");
-        }
-        else
-        ;
-    }
-    close(fp);
+int msgID = msgget (MESSAGE_ID, IPC_CREAT|0666);
+
+buttonInit();
+while(1){
+struct input_event stEvent;	
+int returnValue = 0;
+returnValue = msgrcv(msgID, &stEvent, sizeof(unsigned short) * 2 + sizeof(int), 0, 0);
+		
+if(stEvent.type== EV_KEY){
+printf("EV_KEY(");
+switch(stEvent.code)
+{
+case KEY_VOLUMEUP: printf("Volume up key):"); break;
+case KEY_HOME: printf("Home key):"); break;
+case KEY_SEARCH: printf("Search key):"); break;
+case KEY_BACK: printf("Back key):"); break;
+case KEY_MENU: printf("Menu key):"); break;
+case KEY_VOLUMEDOWN: printf("Volume down key):"); break;
+}
+if ( stEvent.value ) printf("pressed\n");
+else printf("released\n");
+} //End of if
+else // EV_SYN
+; // do notthing
+} // End of While
+
+buttonExit();
 }
