d651243e92e1f55f9feccc757c4ffcb994bf1fa1
bllobll5 <124126192+bllobll5@users.noreply.github.com>
2023-12-03 15:57:38 +0900

    Update lcdtext.c

--- drivertest/textlcd/lcdtext.c
+++ drivertest/textlcd/lcdtext.c
@@ -1,17 +1,43 @@
 #include "lcdtext.h"
+    char textlcd[NUM_ROWS][NUM_COLS] = {};
+	int len;
 
-void lcdtextwrite(const char *str1, const char *str2, int lineFlag) {
-    char textlcd[NUM_ROWS][NUM_COLS + 1] = {0};
+int ledtextinit(void){
+	   lcd_fd = open("/dev/peritextlcd", O_RDWR);
+    if (lcd_fd == -1) {
+        perror("Error opening LCD device");
+        return 1;
+    }
+}
 
+void lcdtextwrite(const char *str1, const char *str2, int lineFlag) {		
     // 전달된 문자열을 textlcd 배열에 복사
     if (lineFlag == 1) {
-        snprintf(textlcd[0], sizeof(textlcd[0]), "%s", str1);
+    	len = strlen(textlcd[0]);
+    	if(len>NUM_COLS)
+        memcpy(str1,textlcd[0],NUM_COLS);
+        else
+        memcpy(str1,textlcd[0],len);
         textlcd[1][0] = '\0';
     } else if (lineFlag == 2) {
-        snprintf(textlcd[1], sizeof(textlcd[1]), "%s", str2);
+        len = strlen(textlcd[1]);
+    	if(len>NUM_COLS)
+        memcpy(str1,textlcd[1],NUM_COLS);
+        else
+        memcpy(str1,textlcd[1],len);
+        
     } else {
         printf("choose 1 or 2\n");
         return;
     }
-
+            // LCD 장치 파일 닫기
+}
+int lcdtextexit(void){
+	textlcd[NUM_ROWS][NUM_COLS] = {};
+	len=0;
+	str1={};
+	str2={};
+	lineFlag=0;
+	close(lcd_fd);
+	return 0;
 }
