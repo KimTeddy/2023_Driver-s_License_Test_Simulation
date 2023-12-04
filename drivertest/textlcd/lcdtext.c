9c4f5c6af549fb30ead65b91b56f4868dd9af2b4
bllobll5 <124126192+bllobll5@users.noreply.github.com>
2023-12-04 11:25:57 +0900

    Update lcdtext.c

--- drivertest/textlcd/lcdtext.c
+++ drivertest/textlcd/lcdtext.c
@@ -15,19 +15,21 @@ int ledtextinit(void){
 void lcdtextwrite(const char *str1, const char *str2, int lineFlag) {		
     // 전달된 문자열을 textlcd 배열에 복사
     if (lineFlag == 1) {
-    	len = strlen(textlcd[0]);
+    	len = strlen(str1);
     	if(len>NUM_COLS)
-        memcpy(str1,textlcd[0],NUM_COLS);
-        else
-        memcpy(str1,textlcd[0],len);
+        memcpy(textlcd[0],str1,NUM_COLS);
+        else{
+        memcpy(textlcd[0],str1,len);
+		textlcd[0][len] = '\0';
+	}
         textlcd[1][0] = '\0';
     } else if (lineFlag == 2) {
-        len = strlen(textlcd[1]);
+        len = strlen(str2);
     	if(len>NUM_COLS)
-        memcpy(str1,textlcd[1],NUM_COLS);
+        memcpy(textlcd[1],str2,NUM_COLS);
         else
-        memcpy(str1,textlcd[1],len);
-        
+	{ memcpy(textlcd[1],str2,len);
+         textlcd[1][len] = '\0'; // Null-terminate the string}
     } else {
         printf("choose 1 or 2\n");
         return;
@@ -35,10 +37,9 @@ void lcdtextwrite(const char *str1, const char *str2, int lineFlag) {
             // LCD 장치 파일 닫기
 }
 int lcdtextexit(void){
-	textlcd[NUM_ROWS][NUM_COLS] = {};
+	 textlcd[0][0] = '\0';
+    textlcd[1][0] = '\0';
 	len=0;
-	str1={};
-	str2={};
 	lineFlag=0;
 	close(lcd_fd);
 	return 0;
