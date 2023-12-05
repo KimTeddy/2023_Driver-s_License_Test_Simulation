97f276aef04a2033356e1e2edfddd34b31fa3366
bllobll5 <124126192+bllobll5@users.noreply.github.com>
2023-12-05 17:49:58 +0900

    Update textlcdtest.c

--- drivertest/textlcd/textlcdtest.c
+++ drivertest/textlcd/textlcdtest.c
@@ -22,30 +22,22 @@ ledtextinit();
 
 	if(str[0]=='\0'){printf("문자열을 입력하세요"); exit(1);}
 
-	size_t len = strlen(str); //개행문자 제거
-    if (len > 0 && str[len - 1] == '\n') {
-        str[len - 1] = '\0';
-    }
     // 입력 문자열을 순회하면서 따옴표를 제거
    
-    char output[sizeof(str)];  // output 배열은 충분한 크기로 선언
-    char *output_ptr = output;
-    char *str_ptr = str;
+    for (; str!= '\0'; str++)//종료 문자를 만날 때까지 반복
+	    { 
+	    if(str == '\'')//( ' )와 같은 문자일 때 
+	{ 
+	strcpy(str, str + 1);
+	str--; 
+	}
+}
 
-    while (*str_ptr) {
-        if (*str_ptr != '\'') {
-            *output_ptr = *str_ptr;
-            output_ptr++;
-        }
-        str_ptr++;
-    }
-    *output_ptr = '\0';
-    
 	if(lineFlag == 1) {
-		strcpy(str1, output);;
+		strcpy(str1, str);
 	}
 	else if(lineFlag == 2) {
-		strcpy(str2, output);;
+		strcpy(str2, str);
 	}
       // lcdtextwrite 함수 호출
     lcdtextwrite(str1, str2, lineFlag);
