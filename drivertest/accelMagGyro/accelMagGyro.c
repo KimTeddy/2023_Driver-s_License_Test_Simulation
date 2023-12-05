27b808236d808854b9f18ff0399d19e50cedcabb
TeddyElectronics <kyc20126@gmail.com>
2023-12-05 19:32:54 +0900

    accelMagGyro 시작

---
+++ drivertest/accelMagGyro/accelMagGyro.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define ACCELPATH "/sys/class/misc/FreescaleAccelerometer/"
+#define MAGNEPATH "/sys/class/misc/FreescaleMagnetometer/"
+#define GYROPATH "/sys/class/misc/FreescaleGyroscope/"
+
+int fd = 0;
+FILE *fp = NULL;
+
+void getAccel(int accel[]){
+    // Accelerometer
+    fd = open(ACCELPATH "enable", O_WRONLY);
+    dprintf(fd, "1");
+    close(fd);
+
+    fp = fopen(ACCELPATH "data", "rt");
+    fscanf(fp, "%d, %d, %d", &accel[0], &accel[1], &accel[2]);
+    printf("I read Accel %d, %d, %d\r\n", accel[0], accel[1], accel[2]);
+    fclose(fp);
+}
+
+void getMagnet(int magnet[]){
+    // Magnetometer
+    fd = open(MAGNEPATH "enable", O_WRONLY);
+    dprintf(fd, "1");
+    close(fd);
+
+    fp = fopen(MAGNEPATH "data", "rt");
+    fscanf(fp, "%d, %d, %d", &magnet[0], &magnet[1], &magnet[2]);
+    printf("I read Magneto %d, %d, %d\r\n", magnet[0], magnet[1], magnet[2]);
+    fclose(fp);
+}
+
+void getGyro(int gyro[]){
+    // Gyroscope
+    fd = open(GYROPATH "enable", O_WRONLY);
+    dprintf(fd, "1");
+    close(fd);
+
+    fp = fopen(GYROPATH "data", "rt");
+    fscanf(fp, "%d, %d, %d", &gyro[0], &gyro[1], &gyro[2]);
+    printf("I read Gyroscope %d, %d, %d\r\n", gyro[0], gyro[1], gyro[2]);
+    fclose(fp);
+}
