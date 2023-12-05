541e8548e263fba5d2abe70ff5b97a8ecf88c594
JW-LEE-1015 <149313973+JW-LEE-1015@users.noreply.github.com>
2023-12-05 18:19:21 +0900

    temp complete?

--- drivertest/temp/temptest.c
+++ drivertest/temp/temptest.c
@@ -2,24 +2,8 @@
 
 int main(int argc, char **argv)
 {
-    char *buffer;
-    int file;
-    buffer = (char *)spi_read_lm74(file);
-    close(file);
-
-    int value = 0;
-    value = (buffer[1] >> 3);
-    value += (buffer[0]) << 5;
-
-    if( buffer[0] & 0x80)
-    {
-        int i = 0;
-        for(i = 31; i > 12; i--)
-        value |= (1 << i);
-    }
-
-    double temp = (double)value * 0.0625;
-    printf("Current Temp : %lf \n", temp);
-
+    printf("Getting Temp!\n");
+    temp_read();
+    
     return 0;
 }
