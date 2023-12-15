a8a6a88b31a221c634c09ec151bfa8f2101012b6
JW-LEE-1015 <149313973+JW-LEE-1015@users.noreply.github.com>
2023-12-15 15:55:18 +0900

    ㅂ

--- drivertest/textlcd/text_lcd/textlcdtest.c
+++ drivertest/textlcd/text_lcd/textlcdtest.c
@@ -5,7 +5,7 @@ int main(){
   const char* str1 = "hello";
   const char* str2 = "embedded system";
   txtlcd_Init();
-  lcdtextwrite( "PRESS START", , 1);
+  lcdtextwrite( "PRESS START", "0xff", 1);
   sleep(1);
   lcdtextwrite( "PRESS START", "0xff", 2);
  // txtlcd_off();
