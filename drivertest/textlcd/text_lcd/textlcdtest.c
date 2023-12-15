3775125cc30eadf67c2f3e6e0857a5a962f239bc
JW-LEE-1015 <149313973+JW-LEE-1015@users.noreply.github.com>
2023-12-15 15:54:14 +0900

    함정이었네요

--- drivertest/textlcd/text_lcd/textlcdtest.c
+++ drivertest/textlcd/text_lcd/textlcdtest.c
@@ -5,9 +5,9 @@ int main(){
   const char* str1 = "hello";
   const char* str2 = "embedded system";
   txtlcd_Init();
-  lcdtextwrite(str1, str2,1);
+  lcdtextwrite( "PRESS START", , 1);
   sleep(1);
-  lcdtextwrite(str1, str2,2);
+  lcdtextwrite( "PRESS START", "0xff", 2);
  // txtlcd_off();
  // txtlcd_Init();
  // sleep(1);
