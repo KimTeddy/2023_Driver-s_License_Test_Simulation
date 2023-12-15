3a7be164cdddf5025fd807f2637b640d106bb36d
JW-LEE-1015 <149313973+JW-LEE-1015@users.noreply.github.com>
2023-12-15 16:29:58 +0900

    ss

--- drivertest/textlcd/text_lcd/textlcdtest.c
+++ drivertest/textlcd/text_lcd/textlcdtest.c
@@ -5,24 +5,31 @@ int main(){
   const char* str1 = "hello";
   const char* str2 = "embedded system";
   txtlcd_Init();
-  lcdtextwrite( " ", "0xff", 1);
-  sleep(1);
+  lcdtextwrite( "PRESS START", "CAR S", 1);
   lcdtextwrite( "PRESS START", "CAR S", 2);
   sleep(1);
+  lcdtextwrite( "BEGIN", "CAR SI", 1);
   lcdtextwrite( "BEGIN", "CAR SI", 2);
   sleep(1);
+  lcdtextwrite( "CAR COMPONENT", "CAR SIM", 1);
   lcdtextwrite( "CAR COMPONENT", "CAR SIM", 2);
-    sleep(1);
+  sleep(1);
+  lcdtextwrite( "UP HILL", "CAR SIMU", 1);
   lcdtextwrite( "UP HILL", "CAR SIMU", 2);
     sleep(1);
+  lcdtextwrite( "JUNCTION", "CAR SIMUL", 1);
   lcdtextwrite( "JUNCTION", "CAR SIMUL", 2);
     sleep(1);
+  lcdtextwrite( "PARKING", "CAR SIMULA", 1);
   lcdtextwrite( "PARKING", "CAR SIMULA", 2);
     sleep(1);
+  lcdtextwrite( "EMERGENCY", "CAR SIMULAT", 1);
   lcdtextwrite( "EMERGENCY", "CAR SIMULAT", 2);
     sleep(1);
+  lcdtextwrite( "ACCELATE", "CAR SIMULATO", 1);
   lcdtextwrite( "ACCELATE", "CAR SIMULATO", 2);
     sleep(1);
+  lcdtextwrite( "END", "CAR SIMULATOR", 1);
   lcdtextwrite( "END", "CAR SIMULATOR", 2);
 
   //9 단계로 나뉨 CAR SIMULATOR
