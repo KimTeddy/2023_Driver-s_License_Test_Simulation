f91f9d2aef7e873187633a5366c70e47a3efb2f4
JW-LEE-1015 <149313973+JW-LEE-1015@users.noreply.github.com>
2023-12-15 15:34:53 +0900

    1

--- drivertest/textlcd/lcdtext.c
+++ drivertest/textlcd/lcdtext.c
@@ -56,7 +56,7 @@ void lcdtextwrite(const char *str1, const char *str2, int lineFlag)
 void txtlcd_Exit(void)
 {
     //char str[0] = '\0';
-    char *str = '';  // 배열의 모든 요소를 '\0'으로 초기화
+    char *str = " ";  // 배열의 모든 요소를 '\0'으로 초기화
     lcdtextwrite(str,str, 1);
     lcdtextwrite(str,str, 2);
     close(fd);
