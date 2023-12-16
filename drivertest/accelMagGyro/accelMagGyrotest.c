f7746ceda5988714a8880d1327bef5225c560ccb
JW-LEE-1015 <149313973+JW-LEE-1015@users.noreply.github.com>
2023-12-16 12:59:00 +0900

    22

--- drivertest/accelMagGyro/accelMagGyrotest.c
+++ drivertest/accelMagGyro/accelMagGyrotest.c
@@ -6,11 +6,12 @@ int main(void)
     int magnet[3];
     int gyro[3];
 
-    getAccel(accel);
-    getMagnet(magnet);
-    getGyro(gyro);
+
     while(1)
     {
+        getAccel(accel);
+        getMagnet(magnet);
+        getGyro(gyro);
         printf("I read Accel %d, %d, %d\r\n", accel[0], accel[1], accel[2]);
         printf("I read Magneto %d, %d, %d\r\n", magnet[0], magnet[1], magnet[2]);
         printf("I read Gyroscope %d, %d, %d\r\n", gyro[0], gyro[1], gyro[2]);
