a2677f1c407b3306e3b2006b1ed246c992d4f79f
JW-LEE-1015 <149313973+JW-LEE-1015@users.noreply.github.com>
2023-12-16 15:47:42 +0900

    ss

--- drivertest/accelMagGyro/accelMagGyrotest.c
+++ drivertest/accelMagGyro/accelMagGyrotest.c
@@ -18,11 +18,12 @@ void loop() // 각도 계산
     AcY = angle[1] << 8 | angle[1];
     AcZ = angle[2] << 8 | angle[2];
     
+    printf("Accel : %d %d %d \n", angle[0], angle[1], angle[2]);
     angle[1] = atan(-AcX / sqrt( pow( AcY, 2) + pow( AcZ, 2)));
     angle[1] *= RADIAN_TO_DEGREE;
     angle[0] = atan( AcY/ sqrt( pow(AcX, 2) + pow(AcZ, 2)));
     angle[0] *= RADIAN_TO_DEGREE; 
-    printf("\nAngle x : %f , Angle y :%f \n", angle[0], angle[1]);
+    printf("\nAngle x : %d , Angle y :%d \n", angle[0], angle[1]);
 }
 
 
