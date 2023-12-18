14814bc4ab9984fe24988eb4a09bfa0afb510b93
JW-LEE-1015 <149313973+JW-LEE-1015@users.noreply.github.com>
2023-12-18 21:51:42 +0900

    test용 moving

--- drivertest/accelMagGyro/accelMagGyrotest.c
+++ drivertest/accelMagGyro/accelMagGyrotest.c
@@ -44,13 +44,16 @@ int main(void)
     double ang;
     int first_accel[3];
     int second_accel[3];
+    int moving;
+    int moving_l;
+    int moving_r;
     printf("Set Default Value\n");
     getAccel(first_accel);
 
     while(1) //game이 끝날 때 까지 second_accel값 받아서 기울기 계산
     {        // 나중에 1 대신 트리거 변수로 바꾸기.
         
-                  getAccel(second_accel);
+            getAccel(second_accel);
             // printf("Set Changing Value\n");
             if( first_accel[0] - second_accel[0] > 5000 ) 
             { //왼쪽으로 기운 경우 차이가 5000이상 나면 
@@ -59,9 +62,11 @@ int main(void)
                 //rcar += 3; printf("r=%d\n", rcar); break;  왼쪽으로 기울이고 있는 동안 왼쪽으로 이동하도록
                 //왼쪽이 +3
                 printf("  Handle Turn Left \n");
+                moving_l = 1;
                 sleep(1);
             
             }
+
             else if(second_accel[0] - first_accel[0] > 5000)
             { //오른쪽으로 기운 경우 [차이가 5000이상 나면]
 
@@ -69,8 +74,18 @@ int main(void)
                 //오른쪽이 -3
                 // rcar -= 3; printf("r=%d\n", rcar); break; // 오른쪽으로 기울이고 있는 동안 오른쪽으로 이동하도록
                 printf(" Handle Turn Right \n");
+                moving_r = 1;
                 sleep(1);
             }
+
+            else if( !(first_accel[0] - second_accel[0] > 5000) || !(second_accel[0] - first_accel[0] > 5000))
+            {
+              // 핸들이 정위치면 moving_l,r을 0으로 설정.
+                moving_l = 0;
+                moving_r = 0;    
+
+            }
+
             else if( first_accel[2] - second_accel[2] > 4000 && first_accel[2] - second_accel[2] < 8000 )
             { // 뒤로 기울인 경우
 
@@ -78,18 +93,22 @@ int main(void)
                 printf(" Slow Down \n");
                 sleep(1);
             }
+
             else if( first_accel[2] - second_accel[2] > 8000 )
                 { // 뒤로 기울인 각도가 큰 경우 (돌발에서 급 브레이크 밟은 경우)
                     // ~ 차 멈추는 코드 ~
                     printf(" Stop \n");
+                    moving -= 3;
                     sleep(1);
                 }
         
             else if( second_accel[2] - first_accel[2] > 4000 && second_accel[2] - first_accel[2] < 8000)
-            {// 앞으로만 기울인 경우
+            {// 앞으로만 기울인 경우 [  가속도 센서의 z값이 바뀜  ]
 
                 // ~차가 앞으로 진행하는 코드 ~
             
+                moving = 1;
+
                 /*speed = 1;  //조금만 기울인 경우 speed = 1;
                 dxcar = speed * cos((180-rcar) * PI / 180.0); xcar += dxcar;
                 dycar = speed * sin((180-rcar) * PI / 180.0); ycar += dycar;
@@ -98,9 +117,12 @@ int main(void)
                 printf(" Car Moving Forward \n");
                 sleep(1);
             }
+
             else if( second_accel[2] - first_accel[2] > 8000 )
                 { // 앞으로 많이 기울인 경우 (가속 구간에서 가속)
                     // ~ 차 속도를 빠르게 ~
+
+                    moving += 2;   
                     /*
                     speed = 2;  //많이 기울인 경우 speed = 2로 설정하여 가속
                     dxcar = speed * cos((180-rcar) * PI / 180.0); xcar += dxcar;
@@ -110,6 +132,7 @@ int main(void)
                     printf(" Car Accelation! \n");
                     sleep(1);
                 }
+                /*
             else if(first_accel[0] - second_accel[0] > 5000 && second_accel[2] - first_accel[2] > 4000 && second_accel[2] - first_accel[2] < 8000) 
             { 
                 printf(" Going Left! \n");
@@ -122,7 +145,7 @@ int main(void)
                 dxcar = speed * cos((180-rcar) * PI / 180.0); xcar += dxcar;
                 dycar = speed * sin((180-rcar) * PI / 180.0); ycar += dycar;
                 break;
-                */
+                
 
             }
             else if(second_accel[0] - first_accel[0] > 5000 && second_accel[2] - first_accel[2] > 4000 && second_accel[2] - first_accel[2] < 8000) 
@@ -141,7 +164,9 @@ int main(void)
 
             }
             usleep(1);
-    }
-       return 0;
-    }
+            
+            return 0;
+}
+       
+    
  
