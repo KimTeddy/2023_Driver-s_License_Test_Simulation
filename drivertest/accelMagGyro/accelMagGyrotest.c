7e91b540fe185c8ddd859fc82b105de67f4d090f
JW_LEE <baljw021015@naver.com>
2023-12-19 20:10:16 +0900

    55

--- drivertest/accelMagGyro/accelMagGyrotest.c
+++ drivertest/accelMagGyro/accelMagGyrotest.c
@@ -66,7 +66,7 @@ int main(void)
           //  moving_l = 0;
           //  moving_r = 0;
             // printf("Set Changing Value\n");
-            if( first_accel[0] - second_accel[0] > 5000 && !(first_accel[2] - second_accel[2] > 8000 && first_accel[2] - second_accel[2] > 4000 ) ) 
+            if( first_accel[0] - second_accel[0] > 5000 && !(first_accel[2] - second_accel[2] < 9000 && first_accel[2] - second_accel[2] > 4000 ) ) 
             { //오른쪽으로 기운 경우 차이가 5000이상 나면 
               // 그리고 Break_on 정도로 기울이지 않았다면 Handle Turn Right
             
@@ -83,7 +83,7 @@ int main(void)
             
             }
 
-            else if(second_accel[0] - first_accel[0] > 5000 && !(first_accel[2] - second_accel[2] > 8000 && first_accel[2] - second_accel[2] > 4000 ) )
+            else if(second_accel[0] - first_accel[0] > 5000 && !(first_accel[2] - second_accel[2] < 9000 && first_accel[2] - second_accel[2] > 4000 ) )
             { // 왼쪽으로 기운 경우 [차이가 5000이상 나면] 
               // && 뒤로 기울이지 않았다면 Handle Turn Left 출력
 
