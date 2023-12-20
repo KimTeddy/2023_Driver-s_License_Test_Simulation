b4493ef3e2d1e44c1b15a133720e89b44f97ea83
TeddyElectronics <kyc20126@gmail.com>
2023-12-20 16:20:34 +0900

    fnd 점수 애니메이션 시스템 완료

--- new_fnd_score/fnd_score.c
+++ new_fnd_score/fnd_score.c
@@ -17,43 +17,42 @@
 #define FND_DRIVER_NAME "/dev/perifnd"
 #define BLINK_DELAY_MS 500
 
-int mode = 0;	  // static display
-int c_score = 99; // 처음 점수는 만점
-
+// 메인 코드에 넣을 것
+int c_score = 100; // 처음 점수는 만점
 int minuspoint = 0;
 int dispnum = 0;
+
+// 그냥 테스트용 변수(이미 있는 것 포함)
+int simuwork = 1;
 int testfail = 0;
+int fortest = 0;
 void ScoreAnim();
 
 // c_score변수는 100이고 minuspoint 변수에 5를 받았을때
 int main()
 {
-	dispnum = c_score * 1000;
-	int off_main[6] = {1, 1, 1, 0, 0, 0};
-	if (c_score < 100)
-		off_main[0] = 0;
-	if (c_score < 10)
-		off_main[1] = 0;
-	fndDisp_canoff(dispnum, 0b1000, off_main);
-	usleep(900000);
-
+	// usleep(900000);
 	minuspoint = 25;
-	ScoreAnim();
+	while (1)
+	{
+		if (simuwork == 1)
+			dispnum = c_score * 1000;
+		int off_main[6] = {1, 1, 1, 0, 0, 0};
+		if (c_score < 100)
+			off_main[0] = 0;
+		if (c_score < 10)
+			off_main[1] = 0;
+		fndDisp_canoff(dispnum, 0b1000, off_main);
 
-	minuspoint = 5;
-	ScoreAnim();
-	minuspoint = 10;
-	ScoreAnim();
-	minuspoint = 15;
-	ScoreAnim();
-	minuspoint = 45;
-	ScoreAnim();
+		if (minuspoint > 0)
+			ScoreAnim();
+	}
 	return 0;
 }
 
 void ScoreAnim()
 {
-	int off1_backup = 0, off0_backup = 0;
+	int off4_backup = 0, off5_backup = 0;
 	int off[6] = {1, 1, 1, 0, 1, 1};
 	dispnum = c_score * 1000 + minuspoint;
 
@@ -64,34 +63,42 @@ void ScoreAnim()
 	if (c_score < 10)
 		off[1] = 0;
 	fndDisp_canoff(dispnum, 0b1000, off); // (현재점수 -까일점수)
-	off1_backup = off[1];
-	off0_backup = off[0];
+
+	off4_backup = off[4];
+	off5_backup = off[5];
 	for (int i = 0; i < 8; i++)
 	{
-		if (off[1] == 1)
-			off[1] ^= 1;
-		if (off[0] == 1)
-			off[1] ^= 1;
+		if (off4_backup == 1)
+			off[4] ^= 1;
+		if (off5_backup == 1)
+			off[5] ^= 1;
 		fndDisp_canoff(dispnum, 0b1000, off);
 		usleep(250000); // 0.5초
 	}
-	off[1] = off1_backup;
-	off[0] = off0_backup;
+	off[1] = off4_backup;
+	off[0] = off5_backup;
 
 	while (minuspoint > 0)
 	{
+		if (c_score < 100) // 현재 점수가 100보다 작으면 100의자리 LED 꺼짐
+			off[0] = 0;
+		if (c_score < 10) // 현재 점수가 10보다 작으면 10의자리 LED 꺼짐
+			off[1] = 0;
+		if (minuspoint < 10) // 감점할 점수가 10보다 작으면 10의자리 LED 꺼짐
+			off[4] = 0;
+
+		if (fortest)
+		{
+			minuspoint += 5;
+			fortest = 0;
+		}
+
 		dispnum = c_score * 1000 + minuspoint;
 		fndDisp_canoff(dispnum, 0b1000, off);
 		usleep(70000); // 0.07초
 		--minuspoint;
 		--c_score;
 
-		if (minuspoint < 10) // 감점할 점수가 10보다 작으면 10의자리 LED 꺼짐
-			off[4] = 0;
-		if (c_score < 100)
-			off[0] = 0;
-		if (c_score < 10)
-			off[1] = 0;
 		printf("number :%d\n", c_score);
 	}
 
@@ -102,6 +109,21 @@ void ScoreAnim()
 	if (c_score < 80)
 		testfail = 1;
 }
+
+
+// minuspoint = 5;
+// ScoreAnim();
+// fortest = 1;
+// minuspoint = 10;
+// ScoreAnim();
+// minuspoint = 15;
+// ScoreAnim();
+// minuspoint = 45;
+// ScoreAnim();
+
+
+
+
 // while (testfail == 1) // 실격일때, wasted
 // {
 // 	char ch[7] = "wasted";
