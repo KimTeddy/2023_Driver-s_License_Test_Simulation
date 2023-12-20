367aaa2c89801a6087fbcdc0829be3604b31104b
JW_LEE <baljw021015@naver.com>
2023-12-20 14:00:20 +0900

    textlcd time

--- drivertest/textlcd/text_lcd/textlcdtest.c
+++ drivertest/textlcd/text_lcd/textlcdtest.c
@@ -4,34 +4,43 @@ int main(){
   
   const char* str1 = "hello";
   const char* str2 = "embedded system";
-  txtlcd_Init();
-  lcdtextwrite( "PRESS START", "CAR S", 1);
-  lcdtextwrite( "PRESS START", "CAR S", 2);
-  sleep(1);
-  lcdtextwrite( "BEGIN         ", "CAR SI", 1);
-  lcdtextwrite( "BEGIN         ", "CAR SI", 2);
-  sleep(1);
-  lcdtextwrite( "CAR COMPONENT", "CAR SIM", 1);
-  lcdtextwrite( "CAR COMPONENT", "CAR SIM", 2);
-  sleep(1);
-  lcdtextwrite( "UP HILL        ", "CAR SIMU", 1);
-  lcdtextwrite( "UP HILL        ", "CAR SIMU", 2);
-    sleep(1);
-  lcdtextwrite( "JUNCTION   ", "CAR SIMUL", 1);
-  lcdtextwrite( "JUNCTION   ", "CAR SIMUL", 2);
-    sleep(1);
-  lcdtextwrite( "PARKING         ", "CAR SIMULA", 1);
-  lcdtextwrite( "PARKING         ", "CAR SIMULA", 2);
-    sleep(1);
-  lcdtextwrite( "EMERGENCY         ", "CAR SIMULAT", 1);
-  lcdtextwrite( "EMERGENCY         ", "CAR SIMULAT", 2);
-    sleep(1);
-  lcdtextwrite( "ACCELATE         ", "CAR SIMULATO", 1);
-  lcdtextwrite( "ACCELATE         ", "CAR SIMULATO", 2);
-    sleep(1);
-  lcdtextwrite( "END                  ", "CAR SIMULATOR", 1);
-  lcdtextwrite( "END                  ", "CAR SIMULATOR", 2);
+  
+  char str_cnt[100];
+  int cnt = 0;
+  while(1)
+  {
 
+    cnt += 1;
+    sprintf(str_cnt, "%d", cnt);
+    txtlcd_Init();
+    lcdtextwrite( "PRESS START", "CAR S", 1);
+    lcdtextwrite( "PRESS START", str_cnt, 2);
+    sleep(1);
+    lcdtextwrite( "BEGIN         ", "CAR SI", 1);
+    lcdtextwrite( "BEGIN         ", str_cnt, 2);
+    sleep(1);
+    lcdtextwrite( "CAR COMPONENT", "CAR SIM", 1);
+    lcdtextwrite( "CAR COMPONENT", str_cnt, 2);
+    sleep(1);
+    lcdtextwrite( "UP HILL        ", "CAR SIMU", 1);
+    lcdtextwrite( "UP HILL        ", str_cnt, 2);
+    sleep(1);
+    lcdtextwrite( "JUNCTION   ", "CAR SIMUL", 1);
+    lcdtextwrite( "JUNCTION   ", str_cnt, 2);
+    sleep(1);
+    lcdtextwrite( "PARKING         ", "CAR SIMULA", 1);
+    lcdtextwrite( "PARKING         ", str_cnt, 2);
+    sleep(1);
+    lcdtextwrite( "EMERGENCY         ", "CAR SIMULAT", 1);
+    lcdtextwrite( "EMERGENCY         ", str_cnt, 2);
+    sleep(1);
+    lcdtextwrite( "ACCELATE         ", "CAR SIMULATO", 1);
+    lcdtextwrite( "ACCELATE         ", str_cnt, 2);
+    sleep(1);
+    lcdtextwrite( "END                  ", "CAR SIMULATOR", 1);
+    lcdtextwrite( "END                  ", str_cnt, 2);
+
+ }
   //9 단계로 나뉨 CAR SIMULATOR
  // txtlcd_off();
  // txtlcd_Init();
