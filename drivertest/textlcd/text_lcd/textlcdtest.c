c81ec2210c0fef4f9f3e96d7d5a0795253a8a8d8
JW_LEE <baljw021015@naver.com>
2023-12-20 14:38:40 +0900

    testing

--- drivertest/textlcd/text_lcd/textlcdtest.c
+++ drivertest/textlcd/text_lcd/textlcdtest.c
@@ -2,63 +2,155 @@
 
 int main(){
   
+  //BASIC UP JUNCTION PARIKNG EMERGENCY ACCEL END
+  int CRS_MAIN = 0;
+  int CRS_START = 0;
+  int CRS_BASIC = 0;
+  int CRS_UP = 0;
+  int CRS_PARKING = 0;
+  int CRS_EMERGENCY = 0;
+  int CRS_ACCEL = 0;
+  int CRS_END = 0;
   const char* str1 = "hello";
   const char* str2 = "embedded system";
   
   char str_cnt[100];
   int cnt = 0;
-  while(1)
-  {
+while (1)
+	{
+		if(cnt >= 59)
+		{
+			cnt = 100;
+      CRS_MAIN = 1;
+		}
+		else if(cnt >= 159)
+		{
+			cnt = 200;
+      CRS_MAIN = 0;
+      CRS_START = 1;
+		}
+		else if(cnt >= 259)
+		{
+			cnt = 300;
+      CRS_START = 0;
+      CRS_BASIC = 1;
+		}
+		else if(cnt >= 359)
+		{
+			cnt = 400;
+      CRS_BASIC = 0;
+      CRS_UP = 1;
+		}
+		else if(cnt >= 459)
+		{
+			cnt = 500;
+      CRS_UP = 0;
+      CRS_PARKING = 1;
+		}
+		else if(cnt >= 559)
+		{
+			cnt = 600;
+      CRS_PARKING = 0;
+      CRS_EMERGENCY = 1;
+    }
+		else if(cnt >= 659)
+		{
+			cnt = 700
+      CRS_EMERGENCY = 0;
+      CRS_ACCEL = 1;
+		}
+		else if(cnt >= 759)
+		{
+			cnt = 800;
+      CRS_ACCEL = 0;
+      CRS_END = 1;
+		}
+		else if(cnt = 859)
+		{
+			cnt = 900;
+      CRS_END = 0;
+		}
+		else if(cnt >= 959)
+		{
+			cnt = 1000;
+		}
+		else if(cnt >= 1059)
+		{
+			cnt = 0;
+		}
+		
+		cnt += 1;
+		sprintf(str_cnt, "              %d", cnt);
+		usleep(5000000);
+		
+		if (CRS_MAIN)
+		{
+		  lcdtextwrite( "PRESS START", "CAR S", 1);
+			lcdtextwrite( "PRESS START", str_cnt, 2);
+ 			// lcdtextwrite( "PRESS START", "CAR S", 2);
+			//MAIN에서는 LCD 윗단에는 "PRESS START" 출력, 
+			
+		}
+		else if (CRS_START)	
+		{
+			lcdtextwrite( "BEGIN         ", "CAR SI", 1);
+			lcdtextwrite( "BEGIN         ", str_cnt, 2);
+  			//lcdtextwrite( "BEGIN         ", "CAR SI", 2);
+			//START부분에서는 윗단에 "" 출력,
+			//아랫부분 네모 0개
+		}
+		else if (CRS_BASIC)
+		{
+			lcdtextwrite( "CAR COMPONENT", "CAR SIM", 1);
+			lcdtextwrite( "CAR COMPONENT", str_cnt, 2);
+  			//lcdtextwrite( "CAR COMPONENT", "CAR SIM", 2);
+			//윗단에 "CAR COMPONENT"
+			// 네모 2개
+		}
+		else if (CRS_UP)
+		{
+			lcdtextwrite( "UP HILL        ", "CAR SIMU", 1);
+			lcdtextwrite( "UP HILL        ", str_cnt, 2);
+  			//lcdtextwrite( "UP HILL        ", "CAR SIMU", 2);
+			// 윗단에 "UP HILL" 
+		}
+		else if (CRS_JUNCTION)
+		{
+			lcdtextwrite( "JUNCTION   ", "CAR SIMUL", 1);
+			lcdtextwrite( "JUNCTION   ", str_cnt, 2);
+  			//lcdtextwrite( "JUNCTION   ", "CAR SIMUL", 2);
+			// "JUNCTION"
+		}
+		else if (CRS_PARKING)
+		{
+			lcdtextwrite( "PARKING         ", "CAR SIMULA", 1);
+			lcdtextwrite( "PARKING         ", str_cnt, 2);
+  			//lcdtextwrite( "PARKING         ", "CAR SIMULA", 2);
+			// "PARKING"
+		}
+		else if (CRS_EMERGENCY)
+		{
+			lcdtextwrite( "EMERGENCY         ", "CAR SIMULAT", 1);
+			lcdtextwrite( "EMERGENCY         ", str_cnt, 2);
+  			//lcdtextwrite( "EMERGENCY         ", "CAR SIMULAT", 2);
+			// "EMERGENCY"
+		}
 
-    
-    sprintf(str_cnt, "              %d", cnt);
-    txtlcd_Init();
-    lcdtextwrite( "PRESS START", "CAR S", 1);
-    lcdtextwrite( "PRESS START", str_cnt, 2);
-    sleep(1);
-    cnt += 1;
-    sprintf(str_cnt, "              %d", cnt);
-    lcdtextwrite( "BEGIN         ", "CAR SI", 1);
-    lcdtextwrite( "BEGIN         ", str_cnt, 2);
-    sleep(1);
-    cnt += 1;
-    sprintf(str_cnt, "              %d", cnt);
-    lcdtextwrite( "CAR COMPONENT", "CAR SIM", 1);
-    lcdtextwrite( "CAR COMPONENT", str_cnt, 2);
-    sleep(1);
-    cnt += 1;
-    sprintf(str_cnt, "              %d", cnt);
-    lcdtextwrite( "UP HILL        ", "CAR SIMU", 1);
-    lcdtextwrite( "UP HILL        ", str_cnt, 2);
-    sleep(1);
-    cnt += 1;
-    sprintf(str_cnt, "              %d", cnt);
-    lcdtextwrite( "JUNCTION   ", "CAR SIMUL", 1);
-    lcdtextwrite( "JUNCTION   ", str_cnt, 2);
-    sleep(1);
-    cnt += 1;
-    sprintf(str_cnt, "              %d", cnt);
-    lcdtextwrite( "PARKING         ", "CAR SIMULA", 1);
-    lcdtextwrite( "PARKING         ", str_cnt, 2);
-    sleep(1);
-    cnt += 1;
-    sprintf(str_cnt, "              %d", cnt);
-    lcdtextwrite( "EMERGENCY         ", "CAR SIMULAT", 1);
-    lcdtextwrite( "EMERGENCY         ", str_cnt, 2);
-    sleep(1);
-    cnt += 1;
-    sprintf(str_cnt, "              %d", cnt);
-    lcdtextwrite( "ACCELATE         ", "CAR SIMULATO", 1);
-    lcdtextwrite( "ACCELATE         ", str_cnt, 2);
-    sleep(1);
-    cnt += 1;
-    sprintf(str_cnt, "              %d", cnt);
-    lcdtextwrite( "END                  ", "CAR SIMULATOR", 1);
-    lcdtextwrite( "END                  ", str_cnt, 2);
-    sleep(1);
-    cnt += 1;
-    sprintf(str_cnt, "              %d", cnt);
- }
+		else if (CRS_ACCEL)
+		{
+			lcdtextwrite( "ACCELATE         ", "CAR SIMULATO", 1);
+			lcdtextwrite( "ACCELATE         ", str_cnt, 2);
+  			//lcdtextwrite( "ACCELATE         ", "CAR SIMULATO", 2);
+			// "ACCELERATE"
+		}
+		else if (CRS_END)
+		{
+			lcdtextwrite( "END                  ", "CAR SIMULATOR", 1);
+			lcdtextwrite( "END                  ", str_cnt, 2);
+  			//lcdtextwrite( "END                  ", "CAR SIMULATOR", 2);
+			// "END"
+		}
+	}
   //9 단계로 나뉨 CAR SIMULATOR
  // txtlcd_off();
  // txtlcd_Init();
