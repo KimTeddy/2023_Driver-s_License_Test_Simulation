3178223bcbe69119c6850ae34fa387009fb11581
TeddyElectronics <kyc20126@gmail.com>
2023-12-20 16:00:02 +0900

    fnd_score

--- new_fnd_score/fnd.c
+++ new_fnd_score/fnd.c
@@ -24,7 +24,6 @@ typedef struct FNDWriteDataForm_tag
 }stFndWriteForm, * pStFndWriteForm;
 
 int fnd(int number, char mode) {
-
 	if (mode == MODE_STATIC_DIS) {
 		fndDisp(number, 0);
 	}
@@ -55,12 +54,14 @@ int fnd(int number, char mode) {
 	}
 	return 0;
 }
-int fndDisp_ch(char ch[7]) { //6자리
-
+int fndDisp_ch(char ch[]) { //6자리
+	int fd;
+	int j;
 	stFndWriteForm stWriteData_char;
-	for (int j = 0; j < MAX_FND_NUM; i++) {
-		stWriteData_char.DataValid[i] = 1;
-	}
+
+
+		stWriteData_char.DataValid[j] = 1;
+
 
 	stWriteData_char.DataNumeric[0] = ch[0];
 	stWriteData_char.DataNumeric[1] = ch[1];
@@ -74,6 +75,7 @@ int fndDisp_ch(char ch[7]) { //6자리
 		perror("driver open error.\n");
 		return 0;
 	}
+
 	write(fd, &stWriteData_char, sizeof(stFndWriteForm));
 	close(fd);
 	return 1;
@@ -106,7 +108,33 @@ int fndDisp(int num, int dotflag) {
 	close(fd);
 	return 1;
 }
+int fndDisp_canoff(int num, int dotflag, int offdigit[]) {
+	int fd;
+	int temp, i;
+	stFndWriteForm stWriteData;
 
+	for (i = 0; i < MAX_FND_NUM; i++) {
+		stWriteData.DataDot[i] = (dotflag & (0x1 << i)) ? 1 : 0;
+		stWriteData.DataValid[i] = offdigit[i];
+	}
+	// if 6 fnd
+	temp = num % 1000000; stWriteData.DataNumeric[0] = temp / 100000;
+	temp = num % 100000; stWriteData.DataNumeric[1] = temp / 10000;
+	temp = num % 10000; stWriteData.DataNumeric[2] = temp / 1000;
+	temp = num % 1000; stWriteData.DataNumeric[3] = temp / 100;
+	temp = num % 100; stWriteData.DataNumeric[4] = temp / 10;
+	stWriteData.DataNumeric[5] = num % 10;
+
+	fd = open(FND_DRIVER_NAME, O_RDWR);
+	if (fd < 0) {
+		perror("driver open error.\n");
+		return 0;
+	}
+
+	write(fd, &stWriteData, sizeof(stFndWriteForm));
+	close(fd);
+	return 1;
+}
 int fndOff()
 {
 	int fd, i;
--- new_fnd_score/fnd_score.c
+++ new_fnd_score/fnd_score.c
@@ -12,72 +12,151 @@
 #include <sys/stat.h>
 #include "fnd.h"
 
-#define MAX_FND_NUM			6
-#define FND_DATA_BUFF_LEN	(MAX_FND_NUM + 2)
+#define MAX_FND_NUM 6
+#define FND_DATA_BUFF_LEN (MAX_FND_NUM + 2)
 #define FND_DRIVER_NAME "/dev/perifnd"
 #define BLINK_DELAY_MS 500
-int mode = 0; // static display
-int c_score = 100; //처음 점수는 만점
-int testfail, number, counter;
-char buffer[7];
-char zero[7] = "      ";
-int minuspoint=0;
 
-//c_score변수는 100이고 minuspoint 변수에 5를 받았을때
-int main() {
+int mode = 0;	  // static display
+int c_score = 99; // 처음 점수는 만점
 
-	fndDisp(c_score,0);
-	while (testfail == 1) //실격일때, wasted
-	{
-		char ch[7] = "wasted";
-		fndDisp_ch(ch);
+int minuspoint = 0;
+int dispnum = 0;
+int testfail = 0;
+void ScoreAnim();
 
-	}
-	while (minuspoint != 0) // minuspoint 변수에 0이 아닌 숫자가 들어온다릿
-	{
-		if (minuspoint == 5) //-5 포인트가 되었을때,
-		{
-			for (int i = 0; i < 6; i++) { //0~5까지 6번 돌아
+// c_score변수는 100이고 minuspoint 변수에 5를 받았을때
+int main()
+{
+	dispnum = c_score * 1000;
+	int off_main[6] = {1, 1, 1, 0, 0, 0};
+	if (c_score < 100)
+		off_main[0] = 0;
+	if (c_score < 10)
+		off_main[1] = 0;
+	fndDisp_canoff(dispnum, 0b1000, off_main);
+	usleep(900000);
 
-				snprintf(buffer, sizeof(buffer), "%d -%d", c_score, minuspoint);
-				fndDisp_ch(buffer);
-				usleep(500000); // 0.5초 = 500000 마이크로초
-				fndDisp_ch(zero);
-				fndDisp(minuspoint, 0);
-				usleep(500000); // 0.5초 = 500000 마이크로초
-					minuspoint--;
-					c_score--;
-			}
+	minuspoint = 25;
+	ScoreAnim();
 
-		}
-		else if (minuspoint == 7)
-		{
-			for (int i = 0; i < 7; i++) { //0~6까지 7번 돌아
-
-				snprintf(buffer, sizeof(buffer), "%d -%d", c_score, minuspoint);
-				fndDisp_ch(buffer); //(현재점수 -까일점수)
-				usleep(500000); // 0.5초 = 500000 마이크로초
-				fndDisp_ch(zero);
-				fndDisp(minuspoint, 0); //다 꺼짐
-				usleep(500000); // 0.5초 = 500000 마이크로초
-					minuspoint--;
-					c_score--;
-			}
-		}
-		else if (minuspoint == 10)
-		{
-			for (int i = 0; i < 10; i++) { //0~9까지 10번 돌아
-
-				snprintf(buffer, sizeof(buffer), "%d -%d", c_score, minuspoint);
-				fndDisp_ch(buffer); //(현재점수 -까일점수)
-				usleep(500000); // 0.5초 = 500000 마이크로초
-				fndDisp_ch(zero);
-				fndDisp(minuspoint, 0); //다 꺼짐
-				usleep(500000); // 0.5초 = 500000 마이크로초
-				minuspoint--;
-				c_score--;
-			}
-		}
-
-	}
+	minuspoint = 5;
+	ScoreAnim();
+	minuspoint = 10;
+	ScoreAnim();
+	minuspoint = 15;
+	ScoreAnim();
+	minuspoint = 45;
+	ScoreAnim();
+	return 0;
 }
+
+void ScoreAnim()
+{
+	int off1_backup = 0, off0_backup = 0;
+	int off[6] = {1, 1, 1, 0, 1, 1};
+	dispnum = c_score * 1000 + minuspoint;
+
+	if (minuspoint < 10)
+		off[4] = 0;
+	if (c_score < 100)
+		off[0] = 0;
+	if (c_score < 10)
+		off[1] = 0;
+	fndDisp_canoff(dispnum, 0b1000, off); // (현재점수 -까일점수)
+	off1_backup = off[1];
+	off0_backup = off[0];
+	for (int i = 0; i < 8; i++)
+	{
+		if (off[1] == 1)
+			off[1] ^= 1;
+		if (off[0] == 1)
+			off[1] ^= 1;
+		fndDisp_canoff(dispnum, 0b1000, off);
+		usleep(250000); // 0.5초
+	}
+	off[1] = off1_backup;
+	off[0] = off0_backup;
+
+	while (minuspoint > 0)
+	{
+		dispnum = c_score * 1000 + minuspoint;
+		fndDisp_canoff(dispnum, 0b1000, off);
+		usleep(70000); // 0.07초
+		--minuspoint;
+		--c_score;
+
+		if (minuspoint < 10) // 감점할 점수가 10보다 작으면 10의자리 LED 꺼짐
+			off[4] = 0;
+		if (c_score < 100)
+			off[0] = 0;
+		if (c_score < 10)
+			off[1] = 0;
+		printf("number :%d\n", c_score);
+	}
+
+	dispnum = c_score * 1000;
+	off[5] = 0; // minuspoint 다 처리됐으니 1의 자리도 끄기
+	fndDisp_canoff(dispnum, 0b1000, off);
+
+	if (c_score < 80)
+		testfail = 1;
+}
+// while (testfail == 1) // 실격일때, wasted
+// {
+// 	char ch[7] = "wasted";
+// 	fndDisp_ch(ch);
+// }
+// while (minuspoint != 0) // minuspoint 변수에 0이 아닌 숫자가 들어온다
+//{
+// switch (minuspoint)
+// {
+// case 5: //-5 포인트가 되었을때,
+// {
+// 	for (int i = 0; i < 6; i++)
+// 	{ // 0~5까지 6번 돌아
+
+// 		snprintf(buffer, sizeof(buffer), "%3d-%2d", c_score, minuspoint);
+// 		fndDisp_ch(buffer);
+// 		usleep(500000); // 0.5초 = 500000 마이크로초
+// 		fndDisp_ch(zero);
+// 		fndDisp(minuspoint, 0);
+// 		usleep(500000); // 0.5초 = 500000 마이크로초
+// 		--minuspoint;
+// 		--c_score;
+// 	}
+// }
+// break;
+// case 7:
+// {
+// 	for (int i = 0; i < 7; i++)
+// 	{ // 0~6까지 7번 돌아
+
+// 		snprintf(buffer, sizeof(buffer), "%3d-%2d", c_score, minuspoint);
+// 		fndDisp_ch(buffer); //(현재점수 -까일점수)
+// 		usleep(500000);		// 0.5초 = 500000 마이크로초
+// 		fndDisp_ch(zero);
+// 		fndDisp(minuspoint, 0); // 다 꺼짐
+// 		usleep(500000);			// 0.5초 = 500000 마이크로초
+// 		--minuspoint;
+// 		--c_score;
+// 	}
+// }
+// break;
+// case 10:
+// {
+// 	for (int i = 0; i < 10; i++)
+// 	{ // 0~9까지 10번 돌아
+
+// 		snprintf(buffer, sizeof(buffer), "%3d-%2d", c_score, minuspoint);
+// 		fndDisp_ch(buffer); //(현재점수 -까일점수)
+// 		usleep(500000);		// 0.5초 = 500000 마이크로초
+// 		fndDisp_ch(zero);
+// 		fndDisp(minuspoint, 0); // 다 꺼짐
+// 		usleep(500000);			// 0.5초 = 500000 마이크로초
+// 		--minuspoint;
+// 		--c_score;
+// 	}
+// }
+// break;
+//}
