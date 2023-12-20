9a1708d02546067f44baf1a6ca02820dcad85a60
JW_LEE <baljw021015@naver.com>
2023-12-21 01:19:31 +0900

    CRS_MANUAL 추가

--- test/test.c
+++ test/test.c
@@ -243,70 +243,75 @@ void *txtdisplay(void)
 
 void *count(void)
   {
-    while (1)
+    while ( !(CRS_MANUAL) && !(CRS_MAIN) )
 	  {
       //cnt += 1;
 		  sprintf(str_cnt0, "%d", cnt[0]);
-      sprintf(str_cnt1, "%d", cnt[1]);
-      sprintf(str_cnt2, "%d", cnt[2]);
-      sprintf(str_cnt3, "%d", cnt[3]);
-      sprintf(str_cnt, "           %d %d%d",  cnt[2], cnt[1], cnt[0]);
+          sprintf(str_cnt1, "%d", cnt[1]);
+          sprintf(str_cnt2, "%d", cnt[2]);
+          sprintf(str_cnt3, "%d", cnt[3]);
+          sprintf(str_cnt, "           %d %d%d",  cnt[2], cnt[1], cnt[0]);
 		  usleep(50000);
       
-      lcdtextwrite( str1, str_cnt, 1);
-	    lcdtextwrite( str1, str_cnt, 2);
+          lcdtextwrite( str1, str_cnt, 1);
+	      lcdtextwrite( str1, str_cnt, 2);
 
         
-if (cnt[0] == 9) 
-{
-    cnt[0] = 0;
-    cnt[1]++;
-} 
-else 
-{
-    cnt[0]++;
-}
+    if (cnt[0] == 9) 
+    {
+        cnt[0] = 0;
+        cnt[1]++;
+    }
+    else 
+    {
+        cnt[0]++;
+    }
  // 10초 단위 검사 
-               
+    }          
 
-   	if (CRS_MAIN)
-		  {
-        str1 = "PRESS START";
+   	    if (CRS_MAIN)
+		{
+            str1 = "PRESS START";
 		   //  lcdtextwrite( "PRESS START", "CAR S", 1);
 			 //  lcdtextwrite( "PRESS START", str_cnt, 2);
  			  // lcdtextwrite( "PRESS START", "CAR S", 2);
 			  //MAIN에서는 LCD 윗단에는 "PRESS START" 출력, 
   
 	  	}
-		  else if (CRS_START)	
-		  {
-        str1 = "BEGIN         ";
+        else if(CRS_MANUAL)
+        {
+            str1 = "MANUAL        ";
+
+        }  
+		else if (CRS_START)	
+		{
+            str1 = "BEGIN         ";
 		   //	lcdtextwrite( "BEGIN         ", "CAR SI", 1);
 		   //	lcdtextwrite( "BEGIN         ", str_cnt, 2);
   	  		//lcdtextwrite( "BEGIN         ", "CAR SI", 2);
 		  	//START부분에서는 윗단에 "" 출력,
 		  	//아랫부분 네모 0개
-		  }
-		  else if (CRS_BASIC)
-		  {
-        str1 = "CAR COMPONENT";        
+		}
+		else if (CRS_BASIC)
+		{
+            str1 = "CAR COMPONENT";        
 		   //	lcdtextwrite( "CAR COMPONENT", "CAR SIM", 1);
 		   //	lcdtextwrite( "CAR COMPONENT", str_cnt, 2);
   	  		//lcdtextwrite( "CAR COMPONENT", "CAR SIM", 2);
 		  	//윗단에 "CAR COMPONENT"
 		  	// 네모 2개
-		  }
-		  else if (CRS_UPHILL)
-		  {
-        str1 = "UP HILL        ";   
+		}
+		else if (CRS_UPHILL)
+		{
+            str1 = "UP HILL        ";   
 		   //	lcdtextwrite( "UP HILL        ", "CAR SIMU", 1);
 		   //	lcdtextwrite( "UP HILL        ", str_cnt, 2);
   	  		//lcdtextwrite( "UP HILL        ", "CAR SIMU", 2);
 		  	// 윗단에 "UP HILL" 
-		  }
-		  else if (CRS_JUNCTION_1||CRS_JUNCTION_2)
-		  {
-        str1 = "JUNCTION   ";   
+		}
+		else if (CRS_JUNCTION_1||CRS_JUNCTION_2)
+		{
+            str1 = "JUNCTION   ";   
 	  	 //	lcdtextwrite( "JUNCTION   ", "CAR SIMUL", 1);
 	  	 //	lcdtextwrite( "JUNCTION   ", str_cnt, 2);
     			//lcdtextwrite( "JUNCTION   ", "CAR SIMUL", 2);
@@ -314,37 +319,37 @@ else
 	  	}
 	  	else if (CRS_PARKING)
 	  	{
-        str1 = "PARKING         ";
+            str1 = "PARKING         ";
 	     //	lcdtextwrite( "PARKING         ", "CAR SIMULA", 1);
 			 //  lcdtextwrite( "PARKING         ", str_cnt, 2);
   			//lcdtextwrite( "PARKING         ", "CAR SIMULA", 2);
 			  // "PARKING"
-		  }
-		  else if (CRS_EMERGENCY_A||CRS_EMERGENCY_B||CRS_EMERGENCY_C||CRS_EMERGENCY_D)
-		  {
-        str1 = "EMERGENCY         ";
+		}
+		else if (CRS_EMERGENCY_A||CRS_EMERGENCY_B||CRS_EMERGENCY_C||CRS_EMERGENCY_D)
+		{
+            str1 = "EMERGENCY         ";
 		  // 	lcdtextwrite( "EMERGENCY         ", "CAR SIMULAT", 1);
 		   //	lcdtextwrite( "EMERGENCY         ", str_cnt, 2);
   	  		//lcdtextwrite( "EMERGENCY         ", "CAR SIMULAT", 2);
 		  	// "EMERGENCY"
-		  }
-		  else if (CRS_ACCEL)
-		  {
-        str1 = "ACCELATE         ";
+		}
+		else if (CRS_ACCEL)
+		{
+            str1 = "ACCELATE         ";
 		   //	lcdtextwrite( "ACCELATE         ", "CAR SIMULATO", 1);
 		  // 	lcdtextwrite( "ACCELATE         ", str_cnt, 2);
   	  		//lcdtextwrite( "ACCELATE         ", "CAR SIMULATO", 2);
 		  	// "ACCELERATE"
-		  }
-		  else if (CRS_END)
-		  {
-        str1 = "END                  ";
+		}
+	    else if (CRS_END)
+		{
+            str1 = "END                  ";
 		  //	lcdtextwrite( "END                  ", "CAR SIMULATOR", 1);
 		  //	lcdtextwrite( "END                  ", str_cnt, 2);
   	  		//lcdtextwrite( "END                  ", "CAR SIMULATOR", 2);
 		  	// "END"
-		  }
-  }
+	    }
+  
 }
   
 
