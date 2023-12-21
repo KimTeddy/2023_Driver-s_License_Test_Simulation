1b3c64df32b1a8d8d510ffc0127c16ad7e85772a
JW_LEE <baljw021015@naver.com>
2023-12-21 10:16:29 +0900

    ㅋ

--- test/test.c
+++ test/test.c
@@ -445,22 +445,7 @@ void *textlcd()
                 sec_lcd = 0;
             }
         }
-    
-        // if(cnt[1]== 6)
-        // {
-        //     cnt[2] += 1;
-        //     cnt[1] = 0;
-        //     cnt[0] = 0;
-        // }
-        // else if (cnt[0] == 9) 
-        // {
-        //     cnt[0] = 0;
-        //     cnt[1]++;
-        // }
-        // else 
-        // {
-        //     cnt[0]++;
-        // }
+
         if(now_level != prev_level_lcd){
             if (now_level == CRS_MAIN)
             {
@@ -505,7 +490,7 @@ void *textlcd()
             {
                 str1 = "END                  ";
             }
-            lcdtextwrite( str1, str_cnt, 1);
+            
             prev_level_lcd = now_level;
         }
         // sprintf(str_cnt0, "%d", cnt[0]);
@@ -515,6 +500,7 @@ void *textlcd()
         //sprintf(str_cnt, "            %d:%d%d",  cnt[2], cnt[1], cnt[0]);
         sprintf(str_cnt, "          %2d:%2d",  min_lcd, sec_lcd);
         //sleep(1);       //시연용
+        lcdtextwrite( str1, str_cnt, 1);
         lcdtextwrite( str1, str_cnt, 2);
         usleep(50000);    //테스트용
         //sprintf(str1, "%s", "PRESS START");
